f141198d035ef156a0043c6b5e98a6d98ae58665
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-15 16:15:29 -0500

    mostrar checkbox programas en el formulario de registro

--- resources/views/registro/index.blade.php
+++ resources/views/registro/index.blade.php
@@ -80,6 +80,7 @@
 
         $('#facultades').change(function(){
             facultades = $(this);
+            alertt(facultades.val());
             if ($(this).val() != '') {
                 var formData = new FormData();
                 formData.append('idfacultad',facultades.val());

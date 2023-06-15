7cba6e770ea3c183683f53b729102a022f101405
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-15 12:27:15 -0500

    Modificación plantilla

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">

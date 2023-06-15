f24a02ef5fa186320b3475b4ae089633a1e9c388
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-15 14:23:37 -0500

    Ruta para facultades

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/', function () {
 Route::controller(RegistroController::class)->group(function(){
     Route::get('/registro','index')->name('registro.index');
     Route::post('/registro/roles','roles')->name('registro.roles');
+    Route::post('/registro/facultades','facultades')->name('registro.facultades');
 });

1729855b35f194714701eeebf3c87f7177421b51
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-15 15:00:21 -0500

    añadir el llamado al archivo app.css

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <title>@yield('title')</title>
 </head>

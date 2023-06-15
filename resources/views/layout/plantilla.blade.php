d4f68998ecc0200ac61952be28420b6ac1ef571c
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-15 13:25:18 -0500

    Roles

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ Session::token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <title>@yield('title')</title>

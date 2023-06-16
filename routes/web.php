7fcbbd900cf935c96e1182cd1fabc8b8950d7781
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-16 14:36:49 -0500

    contraseña

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistroController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\nuevacontrasenaControllerController;
+use App\Http\Controllers\contrasenaControllerController;
 
 /*
 |--------------------------------------------------------------------------

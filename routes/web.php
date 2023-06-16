d332f46488b5d3123e5a8bfa9ce3738a5c77e89a
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-16 08:08:52 -0500

    ruta prueba form registro

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,8 @@ Route::controller(RegistroController::class)->group(function(){
     Route::get('/registro/roles','roles')->name('registro.roles');
     Route::post('/registro/facultades','facultades')->name('registro.facultades');
     Route::post('/registro/programas','programas')->name('registro.programas');
+    
+    Route::get('/registroprueba','index')->name('registroprueba.index');
 });
 
 Route::controller(LoginController::class)->group(function(){

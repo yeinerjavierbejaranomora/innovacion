c41c1585548aeea81a57453e29d6d333eab0652e
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-16 14:23:12 -0500

    ['controlador nuevaContraseña']

--- app/Http/Controllers/nuevacontraseñaController.php
+++ app/Http/Controllers/nuevacontraseñaController.php
@@ -5,8 +5,6 @@ use Illuminate\Http\Request;
 class nuevacontraseñaController extends Controller
 {
     public function index() {
-        /*$roles = DB::table('roles')->get();
-        var_dump($roles);*/
         return view('nuevaContraseña.index');
     }
 }

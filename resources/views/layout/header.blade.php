dd329dcd9248f119a9b20a1439e66660fe2fb3f4
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-16 16:32:02 -0500

    [test]

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -16,13 +16,13 @@
     <link rel="stylesheet" href="{{asset('public/css/app.css')}}">
 
 	<!-- Custom fonts for this template-->
-	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="{{asset('general/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{asset('general/css/sb-admin-2.min.css')}}" rel="stylesheet">
 
 
  

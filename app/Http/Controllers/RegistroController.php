63ccb5297910ecfd8b89f17f3acf4123ef06e6e9
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-16 12:05:09 -0500

    Modificación metodo para crear registro de usuario, y creacion modelo usuario

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -34,7 +34,7 @@ class RegistroController extends Controller
     }
 
     public function saveRegistro(UsuarioRegistroRequest $request){
-        //return $request->idfacultad;
+        return $request->programa;
         $Programas = '';
         
         if($request->idfacultad == null):

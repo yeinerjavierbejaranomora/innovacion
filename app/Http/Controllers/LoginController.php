71a39182690bd22a5a9c8866762636d5242fb60b
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-16 15:05:42 -0500

    creación método login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,6 +13,7 @@ class LoginController extends Controller
     }
 
     public function login(Request $request){
+        return $request;
         /*$credenciales = $request->getCredentials();
 
         if(!Auth::validate($credenciales)):

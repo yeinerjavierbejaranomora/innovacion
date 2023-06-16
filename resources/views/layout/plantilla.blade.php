8693643f8eedb875e27d9627621d7f14fd891423
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-16 14:37:06 -0500

    creación método login

---
+++ resources/views/layout/plantilla.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    {{-- <script type="text/javascript" src="{{asset('js/app.js')}}"></script> --}}
+    <title>@yield('title')</title>
+
+</head>
+
+<body>
+@yield('content')
+
+</body>
+
+</html>

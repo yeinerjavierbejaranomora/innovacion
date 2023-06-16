9232c6960dcbe791eaf6462c9b650648692e9adf
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-16 14:56:12 -0500

    creación método login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,7 +5,8 @@
 		<div class="container-login100" style="background-image: url({{asset('public/assets/images/bg-01.jpg')}});">
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
 				<form class="login100-form validate-form" id="miform" action="{{ route('login.login') }}" method="POST">
-					<span class="login100-form-title p-b-49">
+					@csrf
+                    <span class="login100-form-title p-b-49">
 						Sistema de Proyección Ibero
 					</span>
 

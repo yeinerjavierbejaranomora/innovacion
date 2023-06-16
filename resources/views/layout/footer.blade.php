feae37ec41d99849ec53a03d572c4cad86567800
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-16 15:52:23 -0500

    [*plantilla  utilizacion de codigo]

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,36 @@
+
+
+<!-- End of Main Content -->
+
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy;Universidad Iberoamericana 2023</span>
+        </div>
+    </div>
+</footer>
+
+    <!--===============================================================================================-->
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{asset('public/general/jquery/jquery.min.js')}}"></script>
+
+    <script src="{{asset('public/general/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('public/general/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js')}}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{asset('public/general/chart.js/Chart.min.js')}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('public/general/js/demo/chart-area-demo.js')}}"></script>
+
+    <script src="{{asset('public/general/js/demo/chart-pie-demo.js')}}"></script>
+
+</body>
+
+</html>

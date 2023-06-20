5e25575beaaa8be34064eb9ed7abb32de23d2939
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-20 12:59:14 -0500

    prueba

--- app/Http/Controllers/cambioController.php
+++ app/Http/Controllers/cambioController.php
@@ -22,10 +22,10 @@ class cambioController extends Controller
         ['documento','=',$request->documento]])->get();
 
         if($consulta == '[]'){
-            return view('nuevacontraseña.index');
+            return false;
         }
         else{
-            return false;
+            return view('nuevacontraseña.index');
             //return redirect()->route('cambio.index')->with('consultaFallida', 'Usuario no encontrado');
         }
     }

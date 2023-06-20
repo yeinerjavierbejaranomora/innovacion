874a460ce70cedebac0bba57dadf4c4b4ecd922d
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-20 15:19:32 -0500

    form

--- resources/views/reestablecerpassword/nueva.blade.php
+++ resources/views/reestablecerpassword/nueva.blade.php
@@ -28,7 +28,7 @@
                 <div class="container-login100-form-btn">
                     <div class="wrap-login100-form-btn">
                         <div class="login100-form-bgbtn"></div>
-                        <button type="submit" class="login100-form-btn" onclick="return validacion()">
+                        <button type="submit" class="login100-form-btn">
                             Recuperar contraseña
                         </button>
                     </div>

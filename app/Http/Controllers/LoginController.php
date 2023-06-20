66e386ffd7c1e31ac02fb9320d748f1a76d8f61b
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-20 11:27:26 -0500

    [modificacion formulario cambio contraseña y creacion metodo cambioPass]

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -37,7 +37,7 @@ class LoginController extends Controller
     public function cambioPass(CambioPassRequest $request){
         //dd($request->all());
         $user = DB::table('users')->select('users.email','users.password')->where('id','=',$request->id)->get();
-        return $user->email;
+        return $user[0]->email;
         var_dump(Hash::check($request->password_actual,$user['password']));die();
     }
 

45e5a9f0c114ccdb94fa2b544908d37043d0d25a
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-20 12:19:28 -0500

    prueba

--- resources/views/nuevacontraseña/index.blade.php
+++ resources/views/nuevacontraseña/index.blade.php
@@ -6,7 +6,7 @@
 <div class="limiter">
     <div class="container-login100" style="background-image: url({{asset('public/assets/images/bg-01.jpg')}});">
         <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
-            <!--Formulario para cambio de contraseña-->
+            <!--Formulario para recuperar de contraseña-->
             <form action="" method="post" class="login100-form validate-form" id="miForm">
                 @csrf
                 <span class="login100-form-title p-b-49">

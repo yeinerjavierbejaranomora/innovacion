c6ea4bbfc1b3340c9636239359bcb498d538cc56
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-20 11:22:59 -0500

    [documentacion codigo]

--- routes/web.php
+++ routes/web.php
@@ -30,29 +30,55 @@ Route::get('admin/', function () {
     return view('vistas/admin');
 });
 
+/// definimos las rutas para poder  registrar las facultades, roles, programas,etc...
 Route::controller(RegistroController::class)->group(function(){
+    
+    /* ruta para registro de usuario*/
     Route::get('/registro','index')->name('registro.index');
+    
+    /* ruta para registro de roles*/    
     Route::get('/registro/roles','roles')->name('registro.roles');
+    
+    /* ruta para registro de facultades*/
     Route::post('/registro/facultades','facultades')->name('registro.facultades');
+    /* ruta para registro de programas*/    
     Route::post('/registro/programas','programas')->name('registro.programas');
+
+    /* ruta para salvar los registros*/
     Route::post('/registro/save','saveRegistro')->name('registro.saveregistro');
 });
 
+
+/// definimos las rutas para el login de usuarios
 Route::controller(LoginController::class)->group(function(){
+
+    /** ruta para ver el login del sistema */
     Route::get('/login','index')->name('login.index');
+    
+    /**  */
     Route::post('login/login','login')->name('login.login');
+
+
     Route::get('/login/home/','home')->name('login.home');
+
+
     Route::get('/login/cambio/','cambio')->name('login.cambio');
+
+
     Route::post('/login/cambiopass','cambioPass')->name('login.cambiopass');
+
     Route::get('/logout','logout')->name('logout');
+
 });
 
 Route::controller(contrasenaController::class)->group(function(){
     Route::get('/password','index')->name('password.index');
 });
 
+
 Route::controller(cambioController::class)->group(function(){
     Route::get('/cambio','index')->name('cambio.index');
     Route::get('/nueva','nueva')->name('nueva.index');
 });
+
 

5c10ffdb5363b5dc33b5bfcda3c2afe0b3d18080
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-20 11:23:44 -0500

    {definicion de rutas}

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\cambioController;
 Route::get('/', function () {
     //return view('welcome');
 
-    return view('login_prueba/login');
+    return view('login');
 });
 
 

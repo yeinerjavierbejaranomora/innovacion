461ccc5d2f4a0722ef4d3ee243662ed6dfda8b0a
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-20 11:59:06 -0500

    ruta cambio contraseña

--- routes/web.php
+++ routes/web.php
@@ -78,7 +78,7 @@ Route::controller(contrasenaController::class)->group(function(){
 
 Route::controller(cambioController::class)->group(function(){
     Route::get('/cambio','index')->name('cambio.index');
-    Route::post('/cambio/cambio','consultar')->name('cambio.consultar');
+    Route::get('/cambio/cambio','consultar')->name('cambio.consultar');
     Route::get('/nueva','nueva')->name('nueva.index');
 });
 

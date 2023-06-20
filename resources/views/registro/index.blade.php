5db8e0cc07837f730f3cf4644a5ffb21a05988d8
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-20 09:16:46 -0500

    [creacion metodo login]

--- resources/views/registro/index.blade.php
+++ resources/views/registro/index.blade.php
@@ -14,26 +14,26 @@
 
                 <div class="wrap-input100 validate-input m-b-23" data-validate="idBanner es requerido">
                     <span class="label-input100">ID Banner</span>
-                    <input class="input100" type="number" name="idbanner" placeholder="ID Banner" id="idbanner">
+                    <input class="input100" type="number" name="id_banner" placeholder="ID Banner" id="idbanner">
                     <span class="focus-input100" data-symbol="&#xf205;"></span>
                 </div>
-               
+
 
                 <div class="wrap-input100 validate-input m-b-23" data-validate="documento es requerido">
                     <span class="label-input100">Documento de identidad</span>
                     <input class="input100" type="text" name="documento" placeholder="Documento de identidad" id="documento">
                     <span class="focus-input100" data-symbol="&#xf20b;"></span>
-                </div>              
+                </div>
 
                 <div class="wrap-input100 validate-input m-b-23" data-validate="nombre es requerido">
                     <span class="label-input100">Nombre completo</span>
-                    <input class="input100" type="text" name="nombre" placeholder="Nombre completo" id="nombre">
+                    <input class="input100" type="text" name="name" placeholder="Nombre completo" id="nombre">
                     <span class="focus-input100" data-symbol="&#xf20e;"></span>
                 </div>
 
                 <div class="wrap-input100 validate-input m-b-23" data-validate="correo es requerido">
                     <span class="label-input100">Correo electronico</span>
-                    <input class="input100" type="text" name="correo" placeholder="Correo electronco" id="correo">
+                    <input class="input100" type="text" name="email" placeholder="Correo electronco" id="correo">
                     <span class="focus-input100" data-symbol="&#xf15a;"></span>
                 </div>
 
@@ -43,7 +43,7 @@
 
                 <div class="wrap-input100 validate-input m-b-23" data-validate="rol es requerido">
                     <span class="label-input100">Rol</span>
-                    <select class="input100" name="idrol" id="rol">
+                    <select class="input100" name="id_rol" id="rol">
                         <option value="">Seleccione el rol</option>
                     </select>
                     <span class="focus-input100" data-symbol="&#xf2f1;"></span>
@@ -51,7 +51,7 @@
 
                 <div class="wrap-input100 validate-input m-b-23" data-validate="facultades es requerido">
                     <span class="label-input100">Facultad</span>
-                    <select class="input100" name="idfacultad" id="facultades">
+                    <select class="input100" name="id_facultad" id="facultades">
                         <option value="">Seleccione la facultad</option>
                     </select>
                     <span class="focus-input100" data-symbol="&#xf2f1;"></span>
@@ -63,7 +63,7 @@
                     <div id="programas"></div>
                 </div>
                 <br>
-                
+
                     <div class="container-login100-form-btn">
 						<div class="wrap-login100-form-btn">
 							<div class="login100-form-bgbtn"></div>
@@ -77,7 +77,7 @@
 						<h4> Universidad Iberoamericana</h4>
                   		<p>©2023 Todos los derechos reservados.</p>
 					</div>
-                
+
             </form>
         </div>
     </div>

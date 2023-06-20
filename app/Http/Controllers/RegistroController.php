32e45f1c50e2fb4fec1d54fea8b90f810e069284
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-20 09:10:11 -0500

    [creacion metodo login]

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -34,7 +34,7 @@ class RegistroController extends Controller
     }
 
     public function saveRegistro(UsuarioRegistroRequest $request){
-        dd($request->all());
+        return $request;
         $Programas = '';
 
         if($request->idfacultad == null):

12d1c87040abf8f50ef3785e3ee020f9edc1e404
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-20 12:35:59 -0500

    [test]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Session;
 class UserController extends Controller
 {   
     /// crgamos la vista dependiendo el usuario
-    public function index(){
+    public function home(){
 
         return  redirect()->route('login.home');
         //auth()->user()->documento;

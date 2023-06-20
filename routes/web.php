4f1081547b720fa8706d105af425c1bc07d08526
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-20 15:11:16 -0500

    ruta

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,6 @@ Route::controller(contrasenaController::class)->group(function(){
 Route::controller(cambioController::class)->group(function(){
     Route::get('/cambio','index')->name('cambio.index');
     Route::post('/cambio/cambio','consultar')->name('cambio.consultar');
-    Route::get('/nueva','nueva')->name('cambio.nueva');
     Route::post('/nueva/nueva','actualizar')->name('cambio.actualizar');
 });
 

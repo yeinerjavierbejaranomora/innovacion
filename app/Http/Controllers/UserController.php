c46432e8b6f48b18f02342fbedc7470086504d41
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 14:33:55 -0500

    prueba

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -120,7 +120,7 @@ class UserController extends Controller
 
          $roles = DB::table('roles')->select('roles.nombreRol')->where('id','=',$user->id_rol)->get();
          dd ($roles);
-        return view('vistas.perfil', compact('user'),  compact('facultad'), compact('roles'));
+        return view('vistas.perfil', compact('user'),  compact('facultad'), compact("roles"));
     }
 
     public function actualizar(){

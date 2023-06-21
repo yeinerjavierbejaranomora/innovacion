62137bd0cd6d56702afb1b9939377c038aadd79e
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-21 15:17:15 -0500

    [Vista usuarios admin card datatable]

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -56,19 +56,29 @@
                             <div class="table">
                                 <table id="myTable" class="display">
                                     <thead>
-                                        <tr>
-                                            <th>Column 1</th>
-                                            <th>Column 2</th>
-                                        </tr>
-                                    </thead>
+                                        <thead>
+                                            <tr>
+                                                <th>First name</th>
+                                                <th>Last name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Salary</th>
+                                            </tr>
+                                        </thead>
                                     <tbody>
                                         <tr>
-                                            <td>Row 1 Data 1</td>
-                                            <td>Row 1 Data 2</td>
+                                            <td>Tiger</td>
+                                            <td>Nixon</td>
+                                            <td>System Architect</td>
+                                            <td>Edinburgh</td>
+                                            <td>$320,800</td>
                                         </tr>
                                         <tr>
-                                            <td>Row 2 Data 1</td>
-                                            <td>Row 2 Data 2</td>
+                                            <td>Garrett</td>
+                                            <td>Winters</td>
+                                            <td>Accountant</td>
+                                            <td>Tokyo</td>
+                                            <td>$170,750</td>
                                         </tr>
                                     </tbody>
                                 </table>

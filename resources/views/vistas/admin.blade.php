f34a908a2480940d9d85792226767aba6a5d0aac
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-21 10:00:28 -0500

    [test]

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -14,7 +14,19 @@
             <!-- Main Content -->
             <div id="content">
 
-             
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+
+                
+
+                </nav>
+                <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">

c18b37a591c70913ca4e24d04e9341504125c60f
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 11:20:01 -0500

    perfil

--- resources/views/vistas/perfil.blade.php
+++ resources/views/vistas/perfil.blade.php
@@ -154,12 +154,11 @@
                             <div class="card mb-4">
                                 <div class="card-body text-center">
                                     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
-                                    <h5 class="my-3">John Smith</h5>
-                                    <p class="text-muted mb-1">Full Stack Developer</p>
-                                    <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
+                                    <h5 class="my-3">Nombre Usuario</h5>
+                                    <p class="text-muted mb-1">Rol</p>
+                                    <p class="text-muted mb-4">Facultad</p>
                                     <div class="d-flex justify-content-center mb-2">
-                                        <button type="button" class="btn btn-primary">Follow</button>
-                                        <button type="button" class="btn btn-outline-primary ms-1">Message</button>
+                                        <button type="button" class="btn btn-outline-primary ms-1">Actualizar datos</button>
                                     </div>
                                 </div>
                             </div>

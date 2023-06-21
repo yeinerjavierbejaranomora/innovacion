f3e652d796135faf0a28bc08615e885747fde63d
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 14:17:45 -0500

    prueba

--- resources/views/vistas/perfil.blade.php
+++ resources/views/vistas/perfil.blade.php
@@ -216,7 +216,7 @@
                                             <p class="mb-0">Programas</p>
                                         </div>
                                         <div class="col-sm-9">
-                                            <p class="text-muted mb-0"></p>
+                                            <p class="text-muted mb-0">{{$programas}}</p>
                                         </div>
                                     </div>
                                     <hr>

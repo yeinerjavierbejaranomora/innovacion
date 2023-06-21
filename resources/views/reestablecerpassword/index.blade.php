1f5c7c67dc2d8aa65f53e4f5c86419e808284df5
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 08:46:19 -0500

    prueba

--- resources/views/reestablecerpassword/index.blade.php
+++ resources/views/reestablecerpassword/index.blade.php
@@ -33,7 +33,7 @@
                 <div class="container-login100-form-btn">
                     <div class="wrap-login100-form-btn">
                         <div class="login100-form-bgbtn"></div>
-                        <button type="submit" class="login100-form-btn" onclick="return validacion()">
+                        <button type="submit" class="login100-form-btn">
                             Consultar
                         </button>
 

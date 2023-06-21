87d9360d7cdeadeda15fbeb302d4f01e3ba221f2
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 14:19:28 -0500

    prueba

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -115,7 +115,7 @@ class UserController extends Controller
 
          $facultad = DB::table('facultad')->select('facultad.nombre')->where('id','=',$user->id_facultad)->first(); 
          
-         $programas = explode(",",$user->programa);
+         $programas = explode(";",$user->programa);
             dd($programas);
          $rol = DB::table('roles')->select('roles.nombreRol')->where('id','=',$user->id_rol)->get();
          

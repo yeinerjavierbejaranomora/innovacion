dbbc41a851a656e7b656f19141211d138606cca6
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 13:04:17 -0500

    prueba vista perfil

--- resources/views/vistas/perfil.blade.php
+++ resources/views/vistas/perfil.blade.php
@@ -154,7 +154,7 @@
                             <div class="card mb-4">
                                 <div class="card-body text-center">
                                     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
-                                    <h5 class="my-3">Nombre Usuario</h5>
+                                    <h5 class="my-3">{{$user->nombre }}</h5>
                                     <p class="text-muted mb-1">Rol</p>
                                     <p class="text-muted mb-4">Facultad</p>
                                     <div class="d-flex justify-content-center mb-2">

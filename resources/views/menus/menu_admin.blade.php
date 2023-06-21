14defe658dce545078b9eb50149428b4a79513fb
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-21 15:59:10 -0500

    [test]

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -5,11 +5,11 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home.index') }}">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3"></div>
+                <div class="sidebar-brand-text mx-3">{{$datos['rol']}}</div>
             </a>
 
             <!-- Divider -->

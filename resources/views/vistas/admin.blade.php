826dd150c2f2664fafbf34c58a9769f05165b6b3
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-21 13:01:35 -0500

    [*test}

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -57,7 +57,7 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                              Facultad de {{$value->nombre}}</div>
+                                              <h4> Facultad de {{$value->nombre}}</h4></div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">Estudiantes $40,000</div>
                                         </div>
                                         <div class="col-auto">

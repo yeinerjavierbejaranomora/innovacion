789607af975c70863171b61271401c5607c775f4
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-21 12:35:04 -0500

    [ruta usuarios admin]

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -35,7 +35,7 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
 
-                        <a class="collapse-item" href="{{ route('admin.users') }}l">Usuarios</a>
+                        <a class="collapse-item" href="{{ route('admin.users') }}">Usuarios</a>
                         <a class="collapse-item" href="cards.html">Roles</a>
 
                     </div>

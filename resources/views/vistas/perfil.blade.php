8491b2d305f8e5583a449fedb862a93732a2189f
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 16:07:58 -0500

    prueba

--- resources/views/vistas/perfil.blade.php
+++ resources/views/vistas/perfil.blade.php
@@ -141,117 +141,116 @@
 
         </nav>
         <!-- End of Topbar -->
+    </div>
+</div>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <section style="background-color: #eee;">
+        <div class="container py-5">
 
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
-
-            <!-- Page Heading -->
-            <section style="background-color: #eee;">
-                <div class="container py-5">
-
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <div class="card mb-4">
-                                <div class="card-body text-center">
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
-                                    <h5 class="my-3">{{ auth()->user()->nombre }}</h5>
-                                    <p class="text-muted mb-1"> </p>
-                                    <p class="text-muted mb-4">{{ $datos['facultad'] }}</p>
-                                    <div class="d-flex justify-content-center mb-2">
-                                        <button type="button" class="btn btn-outline-primary ms-1">Actualizar datos</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-8">
-                            <div class="card mb-4">
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-sm-3 text-dark">
-                                            <p class="mb-0">Id Banner</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">{{ auth()->user()->id_banner }}</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3 text-dark">
-                                            <p class="mb-0">Documento de identidad</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">{{auth()->user()->documento }}</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3 text-dark">
-                                            <p class="mb-0">Email</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">{{auth()->user()->email }}</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3 text-dark">
-                                            <p class="mb-0">Rol</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">{{ $datos['rol']}}</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3 text-dark">
-                                            <p class="mb-0">Facultad</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">{{$datos['facultad'] }}</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3 text-dark">
-                                            <p class="mb-0">Programas</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">
-                                                @if($datos['programa'])
-                                                @foreach ($datos['programa'] as $key => $value)
-                                                {{$value}} <br>
-                                                @endforeach
-                                                @endif
-                                            </p>
-
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3 text-dark">
-                                            <p class="mb-0">Estado</p>
-                                        </div>
-
-                                        @if (auth()->user()->activo = 1)
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">Activo</p>
-                                        </div>
-                                        @else
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">Inactivo</p>
-                                        </div>
-                                        @endif
-
-                                    </div>
-                                </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="card mb-4">
+                        <div class="card-body text-center">
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
+                            <h5 class="my-3">{{ auth()->user()->nombre }}</h5>
+                            <p class="text-muted mb-1"> </p>
+                            <p class="text-muted mb-4">{{ $datos['facultad'] }}</p>
+                            <div class="d-flex justify-content-center mb-2">
+                                <button type="button" class="btn btn-outline-primary ms-1">Actualizar datos</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
+
+                <div class="col-lg-8">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-3 text-dark">
+                                    <p class="mb-0">Id Banner</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{ auth()->user()->id_banner }}</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3 text-dark">
+                                    <p class="mb-0">Documento de identidad</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{auth()->user()->documento }}</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3 text-dark">
+                                    <p class="mb-0">Email</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{auth()->user()->email }}</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3 text-dark">
+                                    <p class="mb-0">Rol</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{ $datos['rol']}}</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3 text-dark">
+                                    <p class="mb-0">Facultad</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">{{$datos['facultad'] }}</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3 text-dark">
+                                    <p class="mb-0">Programas</p>
+                                </div>
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">
+                                        @if($datos['programa'])
+                                        @foreach ($datos['programa'] as $key => $value)
+                                        {{$value}} <br>
+                                        @endforeach
+                                        @endif
+                                    </p>
+
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3 text-dark">
+                                    <p class="mb-0">Estado</p>
+                                </div>
+
+                                @if (auth()->user()->activo = 1)
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">Activo</p>
+                                </div>
+                                @else
+                                <div class="col-sm-9">
+                                    <p class="text-muted mb-0">Inactivo</p>
+                                </div>
+                                @endif
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    </section>
 </div>
-</div>
+
+
 @include('layout.footer')

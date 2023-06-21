9173606df2c8d6045e33555eb99093bf876e8b34
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-21 14:48:38 -0500

    [Vista usuarios admin card table]

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -1,3 +1,5 @@
+
+
 <!-- incluimos el header para el html -->
 @include('layout.header')
 
@@ -6,120 +8,78 @@
 <!--  creamos el contenido principal body -->
 
 
-<!-- Content Wrapper -->
-<div id="content-wrapper" class="d-flex flex-column">
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
 
-    <!-- Main Content -->
-    <div id="content">
+            <!-- Main Content -->
+            <div id="content">
 
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-            <!-- Sidebar Toggle (Topbar) -->
-            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                <i class="fa fa-bars"></i>
-            </button>
-
-
-            <div class="input-group">
-
-                <div class="input-group-append">
-                    <h3> Bienvenido {{ auth()->user()->nombre }}</h3>
-                </div>
-            </div>
+                    <!-- Sidebar Toggle (Topbar) -->
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
 
 
+                        <div class="input-group">
+
+                            <div class="input-group-append">
+                                <h3> Bienvenido {{auth()->user()->nombre}}</h3>
+                            </div>
+                        </div>
 
 
-        </nav>
-        <!-- End of Topbar -->
 
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
 
-            <!-- Page Heading -->
-            <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Usuarios</h1>
-                {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                </nav>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Usuarios</h1>
+                        {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
-            </div>
+                    </div>
 
-            <!-- Content Row -->
+                    <!-- Content Row -->
 
-            <div class="row">
+                    <div class="row">
 
-                <!-- Area Chart -->
-                <div class="col-xl-12 col-lg-12">
-                    <div class="card shadow mb-4">
-                        <!-- Card Body -->
-                        <div class="card-body">
-                            <div class="chart-area">
-                                <div class="form-outline mb-4">
-                                    <input type="text" class="form-control" id="datatable-search-input" />
-                                    <label class="form-label" for="datatable-search-input">Search</label>
-                                </div>
-                                <div id="datatable">
+                        <!-- Area Chart -->
+                        <div class="col-xl-12 col-lg-12">
+                            <div class="card shadow mb-4">
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <div class="chart-area">
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
+                <!-- /.container-fluid -->
+
             </div>
 
+
         </div>
-        <!-- /.container-fluid -->
-
-    </div>
-
-
-</div>
-<!-- End of Content Wrapper -->
+    <!-- End of Content Wrapper -->
 
 </div>
 <!-- End of Page Wrapper -->
 
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
+   <!-- Scroll to Top Button-->
+   <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
 
 
-<script>
-    const data = {
-  columns: [
-    {
-      label: 'Name',
-      field: 'name'
-    },
-    'Position',
-    'Office',
-    'Age',
-    'Start date',
-    'Salary',
-  ],
-  rows: [
-    ["Tiger Nixon", "System Architect", "Edinburgh", "61", "2011/04/25", "$320,800"],
-    ["Garrett Winters", "Accountant", "Tokyo", "63", "2011/07/25", "$170,750"],
-    ["Ashton Cox", "Junior Technical Author", "San Francisco", "66", "2009/01/12", "$86,000"],
-    ["Cedric Kelly", "Senior Javascript Developer", "Edinburgh", "22", "2012/03/29", "$433,060"],
-    ["Airi Satou", "Accountant", "Tokyo", "33", "2008/11/28", "$162,700"],
-    ["Brielle Williamson", "Integration Specialist", "New York", "61", "2012/12/02", "$372,000"],
-    ["Herrod Chandler", "Sales Assistant", "San Francisco", "59", "2012/08/06", "$137,500"],
-    ["Rhona Davidson", "Integration Specialist", "Tokyo", "55", "2010/10/14", "$327,900"],
-    ["Colleen Hurst", "Javascript Developer", "San Francisco", "39", "2009/09/15", "$205,500"],
-    ["Sonya Frost", "Software Engineer", "Edinburgh", "23", "2008/12/13", "$103,600"],
-    ["Jena Gaines", "Office Manager", "London", "30", "2008/12/19", "$90,560"],
-    ["Quinn Flynn", "Support Lead", "Edinburgh", "22", "2013/03/03", "$342,000"],
-    ["Charde Marshall", "Regional Director", "San Francisco", "36", "2008/10/16", "$470,600"],
-    ["Haley Kennedy", "Senior Marketing Designer", "London", "43", "2012/12/18", "$313,500"]
-  ],
-};
 
-const instance = new mdb.Datatable(document.getElementById('datatable'), data)
-
-document.getElementById('datatable-search-input').addEventListener('input', (e) => {
-  instance.search(e.target.value);
-});
-</script>
-<!-- incluimos el footer -->
-@include('layout.footer')
+ <!-- incluimos el footer -->
+ @include('layout.footer')

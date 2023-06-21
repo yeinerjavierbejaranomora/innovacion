2cbbbdfd9d80ff15880b9ae019a3180f465c90b2
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 13:48:26 -0500

    controlador perfil

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -117,7 +117,7 @@ class UserController extends Controller
          
          $rol = DB::table('roles')->select('roles.nombreRol')->where('id','=',$user->id_rol)->first();
          
-        return view('vistas.perfil', compact('user'), compact('rol'), compact('facultad'));
+        return view('vistas.perfil', compact('user'), compact('facultad'), compact('rol'),);
     }
 
     public function actualizar(){

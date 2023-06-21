f4d7b6dadd1601c42cd4fc0fe4b7a6504e91fa64
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-21 15:23:53 -0500

    [Vista usuarios admin card datatable]

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -107,21 +107,7 @@
 
 <script>
     $(document).ready(function() {
-        $('#example').DataTable({
-            columnDefs: [{
-                    targets: [0],
-                    orderData: [0, 1],
-                },
-                {
-                    targets: [1],
-                    orderData: [1, 0],
-                },
-                {
-                    targets: [4],
-                    orderData: [4, 0],
-                },
-            ],
-        });
+        $('#example').DataTable();
     });
 </script>
 

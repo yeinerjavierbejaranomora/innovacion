b164bc84e19f517a76d65d2511bc6e43b30dde8b
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-21 11:28:33 -0500

    Vista perfil de usuario

--- resources/views/vistas/perfil.blade.php
+++ resources/views/vistas/perfil.blade.php
@@ -210,6 +210,7 @@
                                             <p class="text-muted mb-0">...</p>
                                         </div>
                                     </div>
+                                    <hr>
                                     <div class="row">
                                         <div class="col-sm-3">
                                             <p class="mb-0">Programas</p>
@@ -218,6 +219,7 @@
                                             <p class="text-muted mb-0">...</p>
                                         </div>
                                     </div>
+                                    <hr>
                                     <div class="row">
                                         <div class="col-sm-3">
                                             <p class="mb-0">Estado</p>

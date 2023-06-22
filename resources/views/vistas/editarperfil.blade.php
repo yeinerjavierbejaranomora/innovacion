94d58677ac5d3e8960f22c826bc39ddbc5485650
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-22 11:42:42 -0500

    script

--- resources/views/vistas/editarperfil.blade.php
+++ resources/views/vistas/editarperfil.blade.php
@@ -214,10 +214,7 @@
                                             <p class="mb-0">Programas</p>
                                         </div>
                                         <div class="col-sm-9">
-                                            <p class="text-muted mb-0">
-                                                <input type="text">
-                                            </p>
-
+                                        <div id="programas"></div>
                                         </div>
                                     </div>
                                     <hr>

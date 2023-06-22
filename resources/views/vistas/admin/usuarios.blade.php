98f3c183e59a1c9e2d3fddc5d59a649dc09be089
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-22 16:01:29 -0500

    [vista usuarios admin datatable]

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -92,7 +92,7 @@
                 method: "POST",
                 contentType: "application/json",
                 data: function(d) {
-                    return JSON.stringify(d)
+                    console.log(JSON.stringify(d))
                 },
                 dataSrc: 'result.data'
             },

217f08410995fb3490a2c4e7673d2397427651bc
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-23 15:08:27 -0500

    prueba

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -131,7 +131,7 @@
                     console.log(data);
                     
                     var id = window.btoa(data.id);
-                    $(location).attr('href', "editar" + id);
+                    $(location).attr('href', "editar/" + id);
 
                 })
             }

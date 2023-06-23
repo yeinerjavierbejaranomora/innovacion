3c037d43d4f38209f9f3503e9641e00ff33b2413
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-23 11:33:04 -0500

    botones

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -107,7 +107,7 @@
                         data: 'nombreRol'
                     },
                     {
-                        defaultContent: "<button type='button' class='btn btn-secondary'><i class='fa-regular fa-trash'></i></button> <button type='button' class='btn btn-secondary'></i></button>"
+                        defaultContent: "<button type='button' class='btn btn-secondary'><i class='fa-solid fa-pen-to-square'></i></button> <button type='button' class='btn btn-secondary'><i class='fa-solid fa-pen-to-square'></i></button>"
                     }
                 ],
                 "language": {

a7456ab5ee175b34c3a41be6b282a89b98ede216
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-23 11:50:04 -0500

    [programas editar perfil]

--- resources/views/vistas/editarperfil.blade.php
+++ resources/views/vistas/editarperfil.blade.php
@@ -319,6 +319,7 @@
                     // });
                         for (let i = 0; i < data.length; i++) {
                             const element = data[i];
+                            console.log(element);
                         }
                 })
             }

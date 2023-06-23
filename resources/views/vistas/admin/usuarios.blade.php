d213ea0f92397b6f3ad0a68d5167fe09bad1ab17
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-23 12:44:18 -0500

    datatable

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -117,7 +117,7 @@
                         title:'Editar'                    
                     },
                     {
-                        defaultContent: '<div class="form-check form-switch"><input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"><label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label><div>',
+                        defaultContent: "<input data-id='activo' class='toggle-class' type='checkbox' data-onstyle='success' data-offstyle='danger' data-toggle='toggle' data-on='Active' data-off='Inactive'",
                         title:'Inactivar'
                     }
                 ],

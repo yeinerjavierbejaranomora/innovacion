6fbe60162d34847d4654995afc6dd0b8af58a3cf
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-23 14:55:40 -0500

    debug

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -125,8 +125,8 @@
                 //lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
             });
         }
+        var_dump(table);
     }
-
     
     function obtener_data_editar(tbody, table) {
         $(tbody).on("click", "button.editar", function() {

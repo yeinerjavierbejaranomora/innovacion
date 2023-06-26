842f4ffe73b629f8068144897d190c37a497d4d4
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-26 14:07:01 -0500

    [programas editar usuario]

--- resources/views/vistas/editarperfil.blade.php
+++ resources/views/vistas/editarperfil.blade.php
@@ -313,6 +313,7 @@
             programasSeparados = programas.split(";").map(Number);
 
             id_facultad = $(this);
+            alert(id_facultad.val)
 
             if($(this).val != ''){
                 $.post('{{  route('registro.programas') }}',{

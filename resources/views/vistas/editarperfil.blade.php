1d28a265117f0438aeb113ab1439d3647e4f522b
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-26 08:38:33 -0500

    editar perfil

--- resources/views/vistas/editarperfil.blade.php
+++ resources/views/vistas/editarperfil.blade.php
@@ -300,7 +300,7 @@
 <script>
 
         $('#facultades').each(function(){
-            programas = "{{ $datos['user']->programa }}";
+            programas = "{{ auth()->user()->programa }}";
             programasSeparados = programas.split(";").map(Number);
             id_facultad = $(this);
             if($(this).val != ''){
@@ -329,7 +329,7 @@
         });
 
         $('#facultades').change(function(){
-            programas = "{{ $datos['user']->programa }}";
+            programas = "{{ auth()->user()->programa }}";
             programasSeparados = programas.split(";").map(Number);
             id_facultad = $(this);
             if($(this).val != ''){
@@ -344,7 +344,7 @@
                         }else{
                             $('#programas').append(`<label><input type="checkbox" id="" name="programa[]" value="${data[i]['id']}"> ${data[i]['programa']}</label><br>`);
                         }
-                    }
+                    }5
 
                 })
             }

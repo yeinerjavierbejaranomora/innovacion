247b034b8249657b7ef8ffc1ab3e8370cc3aa2f6
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-26 11:23:10 -0500

    modal

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -90,7 +90,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                            <button type="button" class="crear btn btn-primary" return="agregarFacultad()">Crear</button>
+                            <button type="button" class="crear btn btn-primary" onclick="agregarFacultad()">Crear</button>
                         </div>
                     </div>
                 </div>
@@ -158,9 +158,7 @@
         }
     }
 
-    function agregarFacultad(modal){
-        $('#exampleModal').on("click", "button.crear",function(){
+    function agregarFacultad(){  
             console.log('entre');
-        })
     }
 </script>

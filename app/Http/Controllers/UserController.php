66d0427ef2b46ad0449bfbad44ac52d031607d81
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-26 09:24:17 -0500

    [debug programas]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -195,10 +195,10 @@ class UserController extends Controller
         if ($user->id_facultad != NULL) {
             // *Consulta para obtener el nombre de la facultad según el ID de esta
             $facultad = DB::table('facultad')->select('facultad.nombre')->where('id', '=', $user->id_facultad)->first();
-            dd($facultad->nombre);
             $facultad = $facultad->nombre;
             // *Explode para que muestre los programas por separado
             $programa = trim($user->programa, ';');
+            dd($programa);
             $programas = explode(";", $programa);
             //$programas = explode(";", $user->programa);
             // *Una vez obtenido el arreglo, se procede a obtener el nombre cada uno según su id

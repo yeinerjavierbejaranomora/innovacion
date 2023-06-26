6f8355dada947869ae1946578a92acba9f591639
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-26 09:30:46 -0500

    [debug programas]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -204,6 +204,7 @@ class UserController extends Controller
             // *Explode para que muestre los programas por separado
             $programa = trim($user->programa, ';');
             $programas = explode(",", $programa);
+            dd($programas);
             //$programas = explode(";", $user->programa);
             // *Una vez obtenido el arreglo, se procede a obtener el nombre cada uno según su id
             foreach ($programas as $key => $value) {

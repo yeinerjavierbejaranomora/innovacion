5887a725dd419e5b5ee1f92f14f7978bbb636e35
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-26 09:56:33 -0500

    [debug programas]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -208,6 +208,7 @@ class UserController extends Controller
         // *Obtenemos los datos del usuario*
         $user = auth()->user();
         // *Validación para determinar si el usuario cuenta con una facultad*
+        dd($user->programa);
         if ($user->id_facultad != NULL) {
             // *Consulta para obtener el nombre de la facultad según el ID de esta
             $facultad = DB::table('facultad')->select('facultad.nombre')->where('id', '=', $user->id_facultad)->first();
@@ -215,7 +216,6 @@ class UserController extends Controller
             // *Explode para que muestre los programas por separado
             $programa = trim($user->programa, ';');
             $programas = explode(",", $programa);
-            dd($programas);
             //$programas = explode(";", $user->programa);
             // *Una vez obtenido el arreglo, se procede a obtener el nombre cada uno según su id
             foreach ($programas as $key => $value) {

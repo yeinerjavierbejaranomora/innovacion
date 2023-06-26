683c1a6b99e6c5274aa89bf508490f1f2d88f079
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-26 09:28:08 -0500

    [debug programas]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -207,7 +207,7 @@ class UserController extends Controller
             //$programas = explode(";", $user->programa);
             // *Una vez obtenido el arreglo, se procede a obtener el nombre cada uno según su id
             foreach ($programas as $key => $value) {
-                dd($key);
+                dd($value);
                 $consulta = DB::table('programas')->select('programa')->where('id', '=', $value)->get();
                 $nombre_programas[$value] = $consulta[0]->programa;
             }

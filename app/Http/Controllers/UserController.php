198ea43f93b67b88c7683a4320edf0d49db6c5df
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-26 14:32:18 -0500

    debug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -143,6 +143,7 @@ class UserController extends Controller
 
     public function savefacultad(CrearFacultadRequest $request)
     {
+        dd($request);
         $facultad = DB::table('facultad')->insert([
             'codFacultad'=>$request[0]->codFacultad,
             'nombre'=>$request[0]->nombre,

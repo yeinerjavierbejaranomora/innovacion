ccbc6ee0139465071b66602ede948e89866db4bb
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-27 13:04:42 -0500

    ruta

--- routes/web.php
+++ routes/web.php
@@ -144,7 +144,7 @@ Route::controller(facultadController::class)->group(function(){
     Route::get('/home/getreglas','get_reglas')->middleware('auth','admin')->name('facultad.getreglas');
 
     /** Ruta para ver los programas por facultad */
-    Route::post('/home/{nombre}/{id}', 'facultad')->middleware('auth')->name('facultad.facultad');
+    Route::get('/home/{nombre}/{id}', 'facultad')->middleware('auth')->name('facultad.facultad');
     /** Ruta para traer los programas por facultad */
     Route::post('/home/{facultad}/{id}', 'actualizar')->middleware('auth')->name('facultad.facultad'); 
 });

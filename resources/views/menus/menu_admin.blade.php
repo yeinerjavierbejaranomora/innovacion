cae014add743481cc52719822519a69377ae67b5
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-27 16:57:40 -0500

    [test menu]

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -115,7 +115,7 @@
                     id_facultad=facultad.id;
                     ruta='home/facultad/'+id_facultad;
 
-                    $('#Facultades').prepend('<a class="collapse-item" href="'+ruta+'">'+nombre_facultad+'</a>');
+                    $('#Facultades').append('<a class="collapse-item" href="'+ruta+'">'+nombre_facultad+'</a>');
                 });
             }
 

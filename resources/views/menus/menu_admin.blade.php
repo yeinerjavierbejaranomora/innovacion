49ebfbca693bfa64e7424d867f058b918f06717d
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-27 11:41:14 -0500

    vista reglas de negocio

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -66,7 +66,7 @@
                 <a class="collapse-item" href="{{ route('facultad.maestria') }}">Maestría</a>
                 <a class="collapse-item" href="{{ route('facultad.continua') }}">Educación continua</a>
                 <a class="collapse-item" href="{{ route('facultad.periodos') }}">Periodos</a>
-                <a class="collapse-item" href="#">Reglas de negocio</a>
+                <a class="collapse-item" href="{{ route('facultad.reglas') }}">Reglas de negocio</a>
 
             </div>
         </div>

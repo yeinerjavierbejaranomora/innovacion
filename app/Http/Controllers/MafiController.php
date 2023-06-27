a9dbf0b6a4d189fe4664aff5b53c13fa8a6188e9
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-27 12:12:22 -0500

    [test}

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -72,7 +72,7 @@ class MafiController extends Controller
         ->get();
 
         
-        dd($periodo);
+        dd($periodo[0]->mes);
 
         $consulta_estudiantes ='SELECT id, homologante, programa FROM homologantes WHERE materias_faltantes="OK" AND programado_ciclo1="" AND programado_ciclo2="" AND programa="PCPV" AND marca_ingreso IN (202313, 202333) AND tipo_estudiante!="XXXXX" ORDER BY id ASC LIMIT 20000';
 

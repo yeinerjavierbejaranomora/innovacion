1fce4e6c5190068d93b778588f12fe714e281228
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-27 11:36:55 -0500

    [test]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Mafi;
 use App\Models\Periodo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MafiController extends Controller
 {

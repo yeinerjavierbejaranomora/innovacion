ac26ee7178ac933fb8c05bd58dba4f27b93be1e9
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-27 15:12:28 -0500

    [metodo data mafi chunk]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -49,7 +49,7 @@ class MafiController extends Controller
                 ->orderBy('id')
                 ->get()
                 ->chunk(200);
-                dd($data[0][17]->idbanner);
+                dd($data[17][0]>idbanner);
         foreach($data as $keys => $estudiantes):
             foreach($estudiantes as $key => $value):
                 dd($value->idbanner);

17080e5f3f3e04e3f4656cf7b7207b7a1c9b58af
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-27 14:58:30 -0500

    [metodo data mafi chunk]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -41,17 +41,15 @@ class MafiController extends Controller
         endforeach;
 
         /** Consulta de los datos tabla datMafi */
-        $datos = [];
-        DB::table('datosMafi')
+        $data = DB::table('datosMafi')
                 ->where('estado','<>','Inactivo')
                 ->whereIn('sello',['TIENE RETENCION','TIENE SELLO FINANCIERO'])
                 ->where('autorizado_asistir','LIKE','ACTIVO%')
                 ->whereIn('periodo',[$pregradoCuatrimestral,$pregradoSemestral,$especializacion,$maestria])
                 ->orderBy('id')
-                ->chunk(100, function($data){
-                    $datos[]=$data;
-                });
-                dd($datos);
+                ->get()
+                ->chunk(200);
+        dd($data);
         //$contData = count($data);
         /*$contKeys = 0;
         //$data = (array) $data;

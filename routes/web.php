e554b6f659f59c485a14e91e6e4635adaeefb9c4
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-27 09:30:19 -0500

    ruta e imagen[]

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::controller(UserController::class)->group(function(){
     /** cargamos ña vista para mostarar todos los usuarios */
     Route::get('/home/users','get_users')->middleware('auth','admin')->name('admin.getusers');
     /** cargamos la vista de administracion de facultades */
-    Route::get('/home/amdministracionFacultades','facultadView')->middleware('auth','admin')->name('admin.facultades');
+    Route::get('/home/amdministracionfacultades','facultadView')->middleware('auth','admin')->name('admin.facultades');
     /** cargamos la vista para mostrar todas las facultades */
     Route::get('/home/facultades','get_facultades')->middleware('auth','admin')->name('admin.getfacultades');
     /** para salvar las facultades */

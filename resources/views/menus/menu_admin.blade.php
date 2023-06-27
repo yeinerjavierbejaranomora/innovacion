bd8d1210c82f75e230b68419e026a25737698e28
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-27 09:12:37 -0500

    script

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -109,7 +109,7 @@
             method: 'post',
             success: function(data) {
                 data.forEach(facultad => {
-                    $('#Facultades').prepend(`<a class="collapse item">${facultad.nombre}</a>`);
+                    $('#Facultades').prepend(`<a class="collapse item" href="#">${facultad.nombre}</a>`);
                 });
             }
         });     

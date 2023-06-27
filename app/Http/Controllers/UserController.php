388842de70e92450444c6560da38b29dd20a1c96
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-27 09:08:37 -0500

    [creacion data]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -394,24 +394,5 @@ class UserController extends Controller
      * lo primero es verificar si no se han programado para ninguno de los ciclos  donde tenga materias faltantes y se verifica por el nombre del programa y el periodo activo
 
       */
-    public function Generar_faltantes(){
-
-
-        /** traemos la fecha actual para poder comparar con el periodo */
-        $fechaActual=date('Y-m-d h:i:s');
-        $fechaSegundos=strtotime($fechaActual);
-
-        $dia=date('j',$fechaSegundos);
-        $mes=date('n',$fechaSegundos);
-        $año=date('Y',$fechaSegundos);
-
-        /** consultamos el periodo en la base de datos teniendo en cuenta la fecha actual */
-
-        $periodo='SELECT * FROM `periodo` WHERE  `mes`='+$mes+ '';
-
-        $consulta_estudiantes ='SELECT id, homologante, programa FROM homologantes WHERE materias_faltantes="OK" AND programado_ciclo1="" AND programado_ciclo2="" AND programa="PCPV" AND marca_ingreso IN (202313, 202333) AND tipo_estudiante!="XXXXX" ORDER BY id ASC LIMIT 20000';
-
-        $consulta_estudiantes = 'SELECT id, homologante, programa FROM homologantes WHERE materias_faltantes="OK" AND programado_ciclo1="" AND programado_ciclo2="" AND programa="PCPV" AND marca_ingreso IN (202313, 202333) AND tipo_estudiante!="XXXXX" ORDER BY id ASC LIMIT 20000';
-
-    }
+   
 }

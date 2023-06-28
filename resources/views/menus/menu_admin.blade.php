562ff004fb79d735901a36ae10dd187db29c111e
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-28 09:51:16 -0500

    prueba

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -59,7 +59,7 @@ function facultades(){
             
             <!--Foreach-->
             @foreach ($facultades as $facultad)
-                    <a class="collapse-item" href="{{ route('facultad.facultad', ['id' => encrypt($facultad->id)]) }}">
+                    <a class="collapse-item" href="{{ route('facultad.facultad', ['id' => encrypt($facultad->id)]) }}" return="$facultad">
                     {{$facultad->nombre}}
                     </a>
             @endforeach

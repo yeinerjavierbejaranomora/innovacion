61d5bebbdad91d7527d6fdfb973852bdd3d5454b
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-28 09:31:00 -0500

    [pasa id]

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -59,7 +59,7 @@ function facultades(){
             
             <!--Foreach-->
             @foreach ($facultades as $facultad)
-                    <a class="collapse-item" href="{{ route('facultad.facultad', ['id '=> $facultad->id]) }}">
+                    <a class="collapse-item" href="{{ route('facultad.facultad', ['id' => encrypt($facultad->id)]) }}">
                     {{$facultad->nombre}}
                     </a>
             @endforeach

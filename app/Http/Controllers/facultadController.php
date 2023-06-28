f772744578db9c51228c55e00d277c6245f4ab9b
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-28 12:21:44 -0500

    prueba

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -137,7 +137,7 @@ class facultadController extends Controller
 
     public function facultad(Request $request)
     {
-        $nombre = DB::table('facultad')->select('nombre')->where('idFacultad', '=', $request->id)->get();
+        $nombre = DB::table('facultad')->select('nombre')->where('id', '=', $request->id)->get();
         dd($nombre);
         return view('vistas.admin.facultad',['id'=>$request->id],['nombre'=>$nombre]);
     }

b3e17fd45f3300645138c89f13343002cdbc298b
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-06-28 16:39:06 -0500

    [tets]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -241,7 +241,7 @@ class MafiController extends Controller
             $ciclo2=explode('-',$value->fechaInicioCiclo2);
 
             if($ciclo1[1]=== $mes[1]|| $ciclo2[1]=== $mes[1]){
-                dd($ciclo1[1]);
+                dd($ciclo1[1],$mes);
             }
 
 

447d270292eedc6804e237d7d62fa3b354b71f00
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-28 10:07:45 -0500

    [metodo data mafi insertar datos mafiReplica, insertar en log e indices de cambio]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -70,7 +70,7 @@ class MafiController extends Controller
                 ->chunk(200);
 
         endif;
-        dd($data);
+        return $data;
 
         if (!empty($data)) :
             $primerId = $data[0][0]->id;

ab1b20db386d2ecea6aa0efd1296804d47c90828
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-28 14:15:58 -0500

    debug

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -155,8 +155,8 @@ class facultadController extends Controller
         echo json_encode(array('data' => $facultad));
     }
 
-    public function malla($codigo){
-
+    public function malla(Request $request){
+        dd($request);
         $nombre = DB::table('programas')->select('programa')->where('codprograma', '=', decrypt($codigo))->get();
         return view('visas.admin.malla',['codigo'=>$codigo],['nombre'=>$nombre[0]->nombre]);
     }

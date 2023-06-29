3479c3d4fa564e183d18e531344d579993944cc1
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-29 14:05:07 -0500

    [console.log]

--- resources/views/vistas/admin/programas.blade.php
+++ resources/views/vistas/admin/programas.blade.php
@@ -174,7 +174,7 @@ button.inactivo{
                     if(data.activo == '1'){
                        // $("td:eq(3)",row).html("Activo"),
                         consulta = $("td:eq(4)",row).add("btn-success");
-                        alert(row);
+                        console.log(row);
                     }
                     else{
                        // $("td:eq(3)",row).html("Inactivo"),

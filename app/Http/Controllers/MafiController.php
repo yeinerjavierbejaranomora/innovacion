0037bfd7ba152355974c3ba1ed32392eac544638
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-06-29 08:47:10 -0500

    [metodo insercion datos desde datosMafiReplica a estudiantes debug  array]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -187,7 +187,7 @@ class MafiController extends Controller
                 ->chunk(200);
         else:
         endif;
-            dd($data[25]);
+            dd($data[23]);
         if(str_contains($data[0][3]->tipoestudiante,'TRANSFERENTE EXTERNO')):
             $concultaTrasferente = DB::table('datosMafiReplica')
             ->join('historialAcademico','datosMafiReplica.idbanner','=','historialAcademico.codBanner')

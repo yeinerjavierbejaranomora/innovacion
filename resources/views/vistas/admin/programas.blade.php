8c6ee53342bd2e6700bce05363bf54db6213d976
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-30 16:36:28 -0500

    s

--- resources/views/vistas/admin/programas.blade.php
+++ resources/views/vistas/admin/programas.blade.php
@@ -270,7 +270,7 @@
                 });
             }
 
-             //* Funcion para trear los datos de la tabla facultades y cargar los opciones del select/
+            var ddd ;     //* Funcion para trear los datos de la tabla facultades y cargar los opciones del select/
     function facultades() {
         $.ajax({
             headers: {
@@ -279,7 +279,8 @@
             url: "{{ route('programa.nombresfac') }}",
             method: 'post',
               success: function(data) {
-                return data;                
+                ddd = data;     
+    console.log(data);           
             }
         });
         

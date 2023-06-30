d82266a80b69942037766b22a891c1431182c6d9
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-30 15:31:08 -0500

    []

--- resources/views/vistas/admin/programas.blade.php
+++ resources/views/vistas/admin/programas.blade.php
@@ -289,7 +289,7 @@
                             '<input type="text" id="codprograma" name="codprograma" value="' + data.codprograma + '" class="form-control" placeholder="codprograma"> <br>' +
                             '<input type="text" id="programa" name="programa" value="' + data.programa + '" class="form-control" placeholder="programa">',
                         input: 'select',
-                        inputOptions: nombres,
+                        inputOptions: facultades,
                         inputPlaceholder: data.nombre,
 
                         icon: 'info',

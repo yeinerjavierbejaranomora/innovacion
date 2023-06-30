70d4c1e7718dbca1eb69cec34f6bb66b3ae8289a
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-06-30 17:50:18 -0500

    a

--- resources/views/vistas/admin/programas.blade.php
+++ resources/views/vistas/admin/programas.blade.php
@@ -37,7 +37,7 @@
                     <h3> Bienvenido {{ auth()->user()->nombre }}</h3>
                 </div>
                 <div>
-                    <input type="number" id="facultadEditar" value='' name="facultadEditar">
+                    <input type="text" id="facultadEditar" value='' name="facultadEditar">
                 </div>
             </div>
 

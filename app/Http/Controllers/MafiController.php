b4496d26772095cce91844b6258903802383f198
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-04 11:21:35 -0500

    [metodo insercion datos desde datosMafiReplica a estudiantes sin materias porver ]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -184,7 +184,7 @@ class MafiController extends Controller
             //dd($historial['programa'][0]);
 
             $mallaCurricular = $this->BaseAcademica($historial['programa']);
-            dd($mallaCurricular[0]);
+            dd($mallaCurricular[0][21]);
             foreach ($mallaCurricular as $key => $value) :
                 if(!in_array( $value->codigoCurso,$historial['materias'])):
                     $insertMateriaPorVer = MateriasPorVer::create([

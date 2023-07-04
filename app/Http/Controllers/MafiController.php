94f3e76564fcaa37c8f8490e3c2744b25b375731
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-04 09:04:04 -0500

    [metodo insercion datos desde datosMafiReplica a estudiantes sin materias porver ]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -178,13 +178,13 @@ class MafiController extends Controller
     {
 
         $primerIngreso =  $this->falatntesPrimerIngreso();
-        dd($primerIngreso[0]);
-        /*foreach($primerIngreso as $estudiante):
+        //dd($primerIngreso[0]);
+        foreach($primerIngreso as $estudiante):
 
             $mallaCurricular = $this->BaseAcademica($estudiante->programa);
 
             return $mallaCurricular;
-        endforeach;*/
+        endforeach;
         $this->periodo();
         $log = DB::table('logAplicacion')->where([['accion', '=', 'Insert'], ['tabla_afectada', '=', 'estudiantes']])->orderBy('id', 'desc')->first();
         //return $log;

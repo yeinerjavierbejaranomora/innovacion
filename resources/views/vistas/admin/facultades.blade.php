2aa31630636b01ac524279addcf17b494011546b
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 11:53:55 -0500

    []

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -92,7 +92,7 @@
 
                         var id_programa = $('#mostrar').val(); 
                         console.log(id_programa);
-                        console.log(3);
+                        console.log(4);
                         var xmlhttp = new XMLHttpRequest();
                         var url = "{{ route('programa.estudiantes', ['id'=>"+id_programa+"]) }}";
                         console.log(url);

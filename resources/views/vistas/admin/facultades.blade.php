6e94facd2208cae4cfa115f9f0b1aaa0e99b8e00
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 15:00:12 -0500

    []

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -99,8 +99,7 @@
                     xmlhttp.onreadystatechange = function() {
                         if (this.readyState == 4 && this.status == 200) {
                             var data = JSON.parse(this.responseText);
-                            var table = $('#example').DataTable();
-                           
+                            
                             var table = $('#example').DataTable({
                                 "data": data.data,
                                 "columns": [{

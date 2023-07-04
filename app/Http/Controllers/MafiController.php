f5072c19692fdcc2c407a7fa897789fc6b0b7873
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-04 12:54:39 -0500

    [metodo insercion datos  estudiantes a materias estudiantes antiguos ]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -179,8 +179,8 @@ class MafiController extends Controller
 
         $estudiantesAntiguos = $this->faltantesAntiguos()->chunk(200);
         foreach ($estudiantesAntiguos as $keys => $estudiantes) :
+            dd($estudiantes);
             foreach ($estudiantes as $key => $value) :
-                dd($value);
             endforeach;
         endforeach;
         die();

d4a637e02a9c93dd75946ec8932a2a1de83ee5eb
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 15:13:38 -0500

    [test]

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -89,7 +89,7 @@
                 // * Datatable para mostrar los estudiantes de cada programa *
                 function buscar() {
 
-                    var id_programa = $('#mostrar').val();
+                    var id_programa = $('input[name=mostrar]').val();
                     console.log(id_programa);
 
                     var xmlhttp = new XMLHttpRequest();

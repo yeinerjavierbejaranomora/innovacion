ebe9a46bb7ae326217fe5f2d3a899cdc7c925e36
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 16:07:31 -0500

    test[]

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -54,7 +54,7 @@
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                         <h6> Programa de {{$value->programa}}</h6>
                                     </div>
-                                    <button id="mostrar" name="mostrar" value="{{ $value->id }}" class="mostrar btn btn-warning text-dark">
+                                    <button id="mostrar" name="mostrar" type="input" value="{{ $value->id }}" class="mostrar btn btn-warning text-dark">
                                     Estudiantes </button>
                                 </div>
 

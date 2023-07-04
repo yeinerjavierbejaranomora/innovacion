4c8b64abd0b85efd2d4a2ef9f5d8b348cdf7f898
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 09:14:33 -0500

    dbug

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -250,7 +250,11 @@ class facultadController extends Controller
 
     public function programasUsuario($id_facultad)
     {
-        $id= decrypt($id_facultad);
+        $id = base64_decode(urldecode($id_llegada));
+
+        if (!is_numeric($id)) {
+            $id = decrypt($id_llegada);
+        }
         dd($id);
         $facultad = DB::table('facultad')->where('id','=',$id)->select('nombre')->get();
         $programas = DB::table('programas')->where('idFacultad','=',$id)->select('programa')->get();

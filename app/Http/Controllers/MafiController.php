17f5ef850f978e162aaf5810789eee3604ab4eb9
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-04 11:45:48 -0500

    [metodo insercion datos desde datosMafiReplica a estudiantes  materias porver transferentes ]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -176,7 +176,7 @@ class MafiController extends Controller
 
     public function getDataMafiReplica()
     {
-        $fechaInicio = date('Y-m-d H:i:s');
+        /*$fechaInicio = date('Y-m-d H:i:s');
         $registroMPV = 0;
         $transferente = $this->falatntesTranferentes();
         foreach($transferente as $estudiante):
@@ -223,7 +223,7 @@ class MafiController extends Controller
         endforeach;
         $fechaFin = date('Y-m-d H:i:s');
         return $registroMPV . "-Fecha Inicio: ". $fechaInicio ."Fecha Fin: ". $fechaFin;
-        $this->periodo();
+        $this->periodo();*/
         $log = DB::table('logAplicacion')->where([['accion', '=', 'Insert'], ['tabla_afectada', '=', 'estudiantes']])->orderBy('id', 'desc')->first();
         //return $log;
         if (empty($log)) :

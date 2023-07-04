b3f2a7f458feb1ab6c719b84aa449b49dc311448
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 09:05:34 -0500

    [vista programas por facultad]

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -41,12 +41,12 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Programas facultad de $datos['facultad']</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Programas facultad de {{$datos['facultad']}}</h1>
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
                     </div>
                     <div>
-                        $datos['programas']
+                        {{$datos['programas']}}
                     </div>
                     <!-- Content Row -->
                     <div class="row" id="facultades">

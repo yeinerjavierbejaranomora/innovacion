a67daf0de0e0611d13bcdd428d9b3e3e0e6711a5
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 10:27:36 -0500

    [datatable]

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -1,5 +1,3 @@
-
-
 <!-- incluimos el header para el html -->
 @include('layout.header')
 
@@ -8,66 +6,145 @@
 <!--  creamos el contenido principal body -->
 
 
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
-            <div id="content">
+    <!-- Main Content -->
+    <div id="content">
 
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-
-                        <div class="input-group">
-
-                            <div class="input-group-append">
-                                <h3> Bienvenido {{auth()->user()->nombre}}</h3>
-                            </div>
-                        </div>
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
 
 
+            <div class="input-group">
+
+                <div class="input-group-append">
+                    <h3> Bienvenido {{auth()->user()->nombre}}</h3>
+                </div>
+            </div>
 
 
-                </nav>
-                <!-- End of Topbar -->
 
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Programas facultad de {{$datos['facultad']}}</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-                    </div>
-                    <!-- Content Row -->
-                    <div class="row" id="facultades">
+        </nav>
+        <!-- End of Topbar -->
 
-                    @foreach($datos['programas'] as $key => $value)
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <a href="{{ route('admin.facultades') }}">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                            <h6> Programa de {{$value->programa}}</h6></div>
-                                            <div class=" mb-0 font-weight-bold text-gray-800"><p>Estudiantes $4,000</p></div>
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Programas facultad de {{$datos['facultad']}}</h1>
+                <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+            </div>
+            <!-- Content Row -->
+            <div class="row" id="facultades">
+
+                @foreach($datos['programas'] as $key => $value)
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <a href="{{ route('admin.facultades') }}">
+                        <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            <h6> Programa de {{$value->programa}}</h6>
                                         </div>
-
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                        <div class=" mb-0 font-weight-bold text-gray-800">
+                                            <p>Estudiantes $4,000</p>
                                         </div>
+                                    </div>
+
+                                    <div class="col-auto">
+                                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
                                     </div>
                                 </div>
                             </div>
-                            </a>
                         </div>
-                    @endforeach             
+                    </a>
+                </div>
+                @endforeach
+            </div>
+
+            <!-- Datatable-->
+            <div class="row">
+
+                <!-- Area Chart -->
+                <div class="col-xl-12 col-lg-12">
+                    <div class="card shadow mb-4">
+                        <!-- Card Body -->
+                        <div class="card-body">
+                            <div class="table">
+                                <table id="example" class="display" style="width:100%">
+                                </table>
+                            </div>
+                        </div>
                     </div>
+                </div>
+            </div>
+
+            <script>
+                // * Datatable para mostrar los estudiantes de cada programa *
+                var xmlhttp = new XMLHttpRequest();
+                var url = "{{ route('facultad.getperiodos') }}";
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        var data = JSON.parse(this.responseText);
+                        var table = $('#example').DataTable({
+                            "data": data.data,
+                            "columns": [{
+                                    data: 'periodos',
+                                    title: 'Periodo'
+                                },
+                                {
+                                    data: 'fechaInicioCiclo1',
+                                    title: 'Fecha de inicio ciclo 1'
+                                },
+                                {
+                                    data: 'fechaInicioCiclo2',
+                                    title: 'Fecha de inicio ciclo 2'
+                                },
+                                {
+                                    data: 'fechaInicioTemprano',
+                                    title: 'Fecha inicio temprano'
+                                },
+                                {
+                                    data: 'fechaInicioPeriodo',
+                                    title: 'Fecha inicio periodo'
+                                },
+                                {
+                                    data: 'periodoActivo',
+                                    title: 'Periodo activo'
+                                },
+                                {
+                                    data: 'year',
+                                    title: 'Año'
+                                },
+                                {
+                                    defaultContent: "<button type='button' class='editar btn btn-secondary' data-toggle='modal' data-target='#editar_facultad' data-whatever='modal'><i class='fa-solid fa-pen-to-square'></i></button>",
+                                    title: 'Editar'
+                                },
+                                {
+                                    defaultContent: "<button type='button' class='eliminar btn btn-secondary'><i class='fa-regular fa-square-minus'></i></button>",
+                                    title: 'Eliminar'
+                                },
+                            ],
+                            "language": {
+                                "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+                            },
+                            //lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+                        });
+                        console.log(table);
+                    }
+
+                }
+            </script>
+            @include('layout.footer')

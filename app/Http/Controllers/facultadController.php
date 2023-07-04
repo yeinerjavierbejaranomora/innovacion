1ec092e8abae9495ab84335028f538c0eaf122a6
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 09:25:12 -0500

    dbug

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -252,7 +252,7 @@ class facultadController extends Controller
     {     
        
         $id = DB::table('facultad')->where('nombre','=',$nombre);    
-        dd($nombre);
+        dd($id);
         $facultad = DB::table('facultad')->where('id','=',$id)->select('nombre')->get();
         $programas = DB::table('programas')->where('idFacultad','=',$id)->select('programa')->get();
         $datos= array(

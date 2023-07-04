b3069454cc6e8bf4dc03b96c543bb63b4cb84e7a
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-04 09:44:53 -0500

    []

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -253,14 +253,11 @@ class facultadController extends Controller
         $consulta = DB::table('facultad')->where('nombre','=',$nombre)->get();    
         $id= $consulta[0]->id;
 
-        $programas_tabla = DB::table('programas')->where('idFacultad','=',$id)->get();
+        $programas_tabla = DB::table('programas')->where('idFacultad','=',$id)->select('programa')->get();
 
-        dd($programas_tabla);
-        
-        dd($programas);
         $datos= array(
             'facultad' => $nombre,
-            'programas' => $programas,
+            'programas' => $programas_tabla,
         );
         
         dd($datos);

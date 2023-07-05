efec2d58a758b49f97fba3a37ffc8c7ca3c4e9b0
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-05 16:16:01 -0500

    [crear maestria]

--- resources/views/vistas/admin/maestria.blade.php
+++ resources/views/vistas/admin/maestria.blade.php
@@ -133,7 +133,7 @@ function facultades() {
             method: 'post',
             success: function(data) {
                 data.forEach(facultad => {
-                    $('#nuevaEsp select#codFacultad').append(`<option value="${facultad.id}">${facultad.nombre}</option>`);
+                    $('#nuevaMaestria select#codFacultad').append(`<option value="${facultad.id}">${facultad.nombre}</option>`);
 
                 })
             }

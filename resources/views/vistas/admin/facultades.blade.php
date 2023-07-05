08a7dfa5a10525501a6dd95b3bb963aaa5575cef
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-05 09:00:19 -0500

    []

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -55,8 +55,10 @@
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                         <h6> Programa de {{$value->programa}}</h6>
                                     </div>
+                                    @foreach($estudiantes as $key => $est)
                                     <button id="mostrar" name="mostrar" type="input" value="{{ $value->id }}" class="mostrar btn btn-warning text-dark">
                                     Estudiantes </button>
+                                    @endforeach
                                 </div>
 
                                 <div class="col-auto">

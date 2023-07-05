eeaa11cac2907ee70d8d2d215a018b991786a353
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-05 11:57:15 -0500

    [bg]

--- resources/views/password/index.blade.php
+++ resources/views/password/index.blade.php
@@ -4,7 +4,7 @@
 
 
 <div class="limiter">
-    <div class="container-login100" style="background-image: url({{asset('public/assets/images/bg-01.jpg')}});">
+    <div class="container-login100" style="background-image: url({{asset('public/assets/images/bg-02.jpg')}});">
         <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
             <!--Formulario para cambio de contraseña-->
             <form action="{{ route('login.cambiopass') }}" method="post" class="login100-form validate-form" id="miForm">

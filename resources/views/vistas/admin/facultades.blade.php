6193cab14587c9c1a85aaf98df298fbabee5c1de
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-05 08:40:56 -0500

    dbug

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -44,7 +44,7 @@
             </div>
             <!-- Content Row -->
             <div class="row" id="facultades">
-                {{$datos}}
+                <?php dd($datos); ?>
                 @foreach($datos['programas'] as $key => $value)
                 <!-- Earnings (Monthly) Card Example -->
                 <div class="col-xl-3 col-md-6 mb-4">

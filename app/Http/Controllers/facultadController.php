a4a4c0f78fd81c335b679b6da3573e78f2bd072b
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-05 14:04:08 -0500

    [vista programas]

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -204,13 +204,13 @@ class facultadController extends Controller
         $codigo = $_POST['codPrograma'];
         $nombre = $_POST['nombre'];
         $codFacultad = $_POST['codFacultad'];
-        $tabla = $_POST['tabla'];
+        
 
         $crear = DB::table('programas')->insert([
             'codprograma' => $codigo,
             'programa' => $nombre,
             'idFacultad' => $codFacultad,
-            'tabla' => $tabla,
+            'tabla' => 'pregrado',
         ]);
 
         if ($crear) :

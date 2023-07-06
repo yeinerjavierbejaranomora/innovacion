ebf5e3278786ad8c88ee2b48ecd4dd624744905d
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-06 08:39:16 -0500

    [metodo insercion datos  estudiantes a materias estudiantes transferentes ajustes]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -195,10 +195,10 @@ class MafiController extends Controller
                 $historial = $this->historialAcademico($estudiante->homologante);
                 //dd($historial);
                 $mallaCurricular = $this->BaseAcademica($estudiante->homologante,$estudiante->programa);
-                dd($mallaCurricular);
+                //dd($mallaCurricular);
                 foreach ($mallaCurricular as $key => $malla) :
                     foreach ($malla as $key => $value) :
-                        //dd($value);
+                        dd($value);
                         if (!in_array($value->codigoCurso, $historial['materias'])) :
                             $insertMateriaPorVer = MateriasPorVer::create([
                                 "codBanner"      => $estudiante->homologante,
@@ -734,7 +734,7 @@ class MafiController extends Controller
                 // AND tipo_estudiante!="XXXXX"
                 // ORDER BY id ASC
                 // LIMIT 20000;
-             
+
                 // Estudiantes para generar faltantes por programa
                 $consulta_homologante= DB::table('estudiantes')
                 ->select('id', 'homologante', 'programa')

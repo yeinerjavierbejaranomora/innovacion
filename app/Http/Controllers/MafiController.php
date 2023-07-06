07340fe602cbbfdda7ecfe158c1fbac6d7811ec4
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-06 09:29:22 -0500

    [metodo insercion datos  estudiantes a materias estudiantes transferentes ajustes]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -200,7 +200,7 @@ class MafiController extends Controller
                     return $a['codMateria'] <=> $b['codMateria'];
                 });
                 foreach ($diff as $key => $value) {
-                    dd($value);
+                    dd($value['codBanner']);
                     $insertMateriaPorVer = MateriasPorVer::create([
                         "codBanner"      => $value->codBanner,
                         "codMateria"      => $value->codMateria,

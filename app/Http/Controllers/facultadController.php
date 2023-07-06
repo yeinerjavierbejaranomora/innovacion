255595d405c7f4daec567869e5928c42bc0861fe
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-06 14:36:29 -0500

    test

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -420,7 +420,7 @@ class facultadController extends Controller
         $fecha2 = $_POST['ciclo2'];
         $temprano = $_POST['temprano'];
         $periodo = $_POST['periodo'];
-        $año = $_POST['fecha'];
+        $año = intval($_POST['fecha']);
 
         $crear = DB::table('periodo')->insert([
             'periodos' => $nombre,

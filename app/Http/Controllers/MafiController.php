77270bec0e9088a3756958692f3002446afbe205
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-06 08:46:52 -0500

    [metodo insercion datos  estudiantes a materias estudiantes transferentes ajustes]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -197,8 +197,9 @@ class MafiController extends Controller
                 $mallaCurricular = $this->BaseAcademica($estudiante->homologante,$estudiante->programa);
                 //dd($mallaCurricular);
                 foreach ($mallaCurricular as $key => $malla) :
+                    dd($malla);
                     foreach ($malla as $key => $value) :
-                        dd($value->codMateria);
+                        dd($value);
                         if (!in_array($value->codigoCurso, $historial['materias'])) :
                             $insertMateriaPorVer = MateriasPorVer::create([
                                 "codBanner"      => $estudiante->homologante,

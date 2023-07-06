9b2eab6b0c7531e9b0fc2c9172906adb801194f4
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-06 12:08:59 -0500

    test

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -423,7 +423,6 @@ class facultadController extends Controller
         $ciclo1 = $_POST['ciclo1'];
         $ciclo2 = $_POST['ciclo2'];
 
-        dd($_POST);
         if(empty($ciclo1))
         {
             $ciclo1 = 0;

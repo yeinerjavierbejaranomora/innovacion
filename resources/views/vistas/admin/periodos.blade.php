1f21b0af939d0a44976dd84a3bc9ef979ab325d3
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-06 11:46:09 -0500

    test

--- resources/views/vistas/admin/periodos.blade.php
+++ resources/views/vistas/admin/periodos.blade.php
@@ -260,8 +260,8 @@
                                     temprano: $(document).find('#temprano').val(),
                                     periodo: $(document).find('#periodo').val(),
                                     año: $(document).find('#año').val(),
-                                    ciclo1: $(document).find('#ciclo1').is(':checked') ? 1 : 0,
-                                    ciclo2: $(document).find('#ciclo2').is(':checked') ? 1 : 0,
+                                    ciclo1: $(document).find('#ciclo1').prop(':checked') ? 1 : 0,
+                                    ciclo2: $(document).find('#ciclo2').prop(':checked') ? 1 : 0,
                                 },
                                 function(result) {
                                     console.log(result);

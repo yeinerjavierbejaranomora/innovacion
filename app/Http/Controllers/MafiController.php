b4187b860d1de082839b7f7dd0d0b91d03531a9d
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-06 14:56:28 -0500

    [metodo insercion datos  estudiantes a materias estudiantes antiguos logs]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -179,7 +179,8 @@ class MafiController extends Controller
     {
 
         $estudiantesAntiguos = $this->faltantesAntiguos()->get()->chunk(200);
-        dd($estudiantesAntiguos[12][2579]);
+        dd($estudiantesAntiguos[12][2579]->homologante);
+
         die();
         $estudiantesAntiguos = $this->faltantesAntiguos()->chunk(200, function($estudiantes){
             $registroMPV = 0;

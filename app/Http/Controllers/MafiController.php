e3c1039b568790809f587e87e00a6f6679bb0ca1
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-07-07 09:54:20 -0500

    [metodo materias faltantes primer ingreso, malla curricular]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -185,6 +185,7 @@ class MafiController extends Controller
             $offset = $log->idFin;
         endif;
         $primerIngreso = $this->falatntesPrimerIngreso($offset);
+        dd($primerIngreso);
         if (!empty($primerIngreso)) :
             $fechaInicio = date('Y-m-d H:i:s');
             $registroMPV = 0;

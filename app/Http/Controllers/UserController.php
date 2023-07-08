566e83a8863de70bee6071fdec56cd8ffa48a0dc
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-07 21:48:34 -0500

    []

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -362,14 +362,14 @@ class UserController extends Controller
 
         if ($id === auth()->user()->id) :
             if ($actualizar) :
-                logUsuariosController::editarBasedeDatos(Constantes::ACTUALIZAR, 'Users', json_encode($informacionOriginal), json_encode($request));
+                logUsuariosController::editarBasedeDatos(Constantes::ACTUALIZAR, 'Users', json_encode($informacionOriginal), json_encode($request->parameters));
                 return  redirect()->route('user.perfil', ['id' => encrypt($id)])->with('Sucess', 'Actualizacion exitosa!');
             else :
                 return redirect()->route('user.perfil', ['id' => encrypt($id)])->withErrors('Error', 'Error al actuaizar los datos del usuario');
             endif;
         else :
             if ($actualizar) :
-                logUsuariosController::editarBasedeDatos(Constantes::ACTUALIZAR, 'Users', json_encode($informacionOriginal), json_encode($request));
+                logUsuariosController::editarBasedeDatos(Constantes::ACTUALIZAR, 'Users', json_encode($informacionOriginal), json_encode($request->parameters));
                 return  redirect()->route('admin.users')->with('Sucess', 'Actualizacion exitosa!');
             else :
                 return redirect()->route('admin.users')->withErrors('Error', 'Error al actuaizar los datos del usuario');

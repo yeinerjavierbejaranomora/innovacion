db0aeca46b6de88962ad8f3ca53243e816afd7a0
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-10 09:30:40 -0500

    Form

--- resources/views/reestablecerpassword/cambio.blade.php
+++ resources/views/reestablecerpassword/cambio.blade.php
@@ -112,7 +112,7 @@
             <div class="card mb-4">
                 <div class="card-body text-center">
                     <img src="https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black.png" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
-                    <h5 class="my-3">{{ $datos['user']->nombre }}</h5>
+                    <h5 class="my-3">{{auth()->user()->nombre}}</h5>
                     <p class="text-muted mb-1">{{ $datos['rol'] }}</p>
                     <p class="text-muted mb-4">{{ $datos['facultad'] }}</p>
 

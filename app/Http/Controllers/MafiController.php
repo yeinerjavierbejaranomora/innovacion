0edceb58ba96b47f3272e18486398ee34dcff2ba
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-10 11:48:05 -0500

    [consulta estudiantes]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -185,7 +185,12 @@ class MafiController extends Controller
             $codigoBanner = $estudiante->homologante;
             $programa = $estudiante->programa;
             $materiasPorVer = $this->materiasPorVer($codigoBanner,$programa);
-            dd($materiasPorVer);
+            $numeroCreditos = DB::table('mallaCurricular')
+                                    ->join('planeacion','planeacion.codMateria','=','mallaCurricular.codigoCurso')
+                                    ->where('planeacion.codBanner','=',$codigoBanner)
+                                    ->groupBy('planeacion.codBanner')
+                                    ->dd();
+            dd($numeroCreditos);
 
         endforeach;
         die();

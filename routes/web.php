9dae557ca79cfe7916d13392f3da22a86ce8177e
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-07-11 10:19:02 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::controller(MafiController::class)->group(function(){
     //carga de mafis
     Route::get('/home/admin/mafi','inicioMafi')->middleware('auth','admin')->name('admin.mafi');
     Route::get('/home/admin/datamafi', 'getDataMafi')->middleware('auth','admin')->name('admin.getdatamafi');
-    Route::get('    ', 'getDataMafiReplica')->middleware('auth','admin')->name('admin.getdatamafireplica');
+    Route::get('/home/admin/datamafireplica', 'getDataMafiReplica')->middleware('auth','admin')->name('admin.getdatamafireplica');
     Route::get('/home/admin/periodo', 'periodo')->middleware('auth','admin')->name('admin.periodo');
     Route::get('/home/admin/Generar_faltantes', 'materiasPorVer')->middleware('auth','admin')->name('admin.Generar_faltantes');
 

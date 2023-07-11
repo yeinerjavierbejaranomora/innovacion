9aefd817d55bb4fedb9bb09290d0707f93cae0e6
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-11 15:14:42 -0500

    derechos reservados form

--- resources/views/login/prueba.blade.php
+++ resources/views/login/prueba.blade.php
@@ -18,6 +18,11 @@
 			<div class="align-content-center">
 				<img src="public/assets/images/Escudo.png" width="300" alt="">
 			</div>
+		<br>
+		<div class="text-center text-white">
+			<h4> Universidad Iberoamericana</h4>	
+			<h4> Todos los derechos reservados </h4>
+		</div>
 		</div>
 		<div class="col">
 			<h2 class="fw-light text-center"> Sistema de proyeccion Ibero</h2>

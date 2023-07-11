f7a6dd1849f197ff11f7083c301277173e97965d
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-11 15:50:36 -0500

    titulo

--- resources/views/login/prueba.blade.php
+++ resources/views/login/prueba.blade.php
@@ -45,7 +45,7 @@
 		</div>
 		<div class="col">
 			<br><br>
-			<h2 class="fw-bold text-center mb-5"> Sistema de  Proyección Ibero</h2>
+			<h2 class="text-center mb-5" style="font-weight: 600;"> Sistema de  Proyección Ibero</h2>
 
 			<form action="#" class="align-content-center">
 				<div class="mb-5 col-10 mx-auto">

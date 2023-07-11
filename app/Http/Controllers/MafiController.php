a70e5c9d58c63021403cc4d26473577ecdb54c93
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-07-11 09:54:03 -0500

    test materias por ver

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -180,13 +180,14 @@ class MafiController extends Controller
 
         /**consulta de estudinates primer ciclo */
         $estudiantesPC = $this->programarPrimerCiclo();
+
         foreach($estudiantesPC as $estudiante):
             $idEstudiante = $estudiante->id;
             $codigoBanner = $estudiante->homologante;
             $programa = $estudiante->programa;
             $ruta = $estudiante->bolsa;
             $tipoEstudiante = $estudiante->tipo_estudiante;
-            $materiasPorVer = $this->programacion($codigoBanner,$programa);
+            $materiasPorVer = $this->materiasPorVer($codigoBanner,$ciclo,$programa);
             //dd($materiasPorVer);
             /**select `planeacion`.`codBanner`, SUM(mallaCurricular.creditos) AS CreditosPlaneados from `mallaCurricular` inner join `planeacion` on `planeacion`.`codMateria` = `mallaCurricular`.`codigoCurso` where `planeacion`.`codBanner` = 100074631 group by `planeacion`.`codBanner` */
             $numeroCreditos = DB::table('mallaCurricular')
@@ -1075,6 +1076,9 @@ class MafiController extends Controller
 
         /**Materias por ver de cada estudiante */
         public function materiasPorVer($codBanner,$ciclo,$programa){
+
+            $periodo=$this->periodo();
+            dd($periodo);
 
             // Materias que debe ver el estudiante
             $materiasPorVer = DB::table("materiasPorVer mp")

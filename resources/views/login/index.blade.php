77ae2fe53b0fe76fbd19d19042ecb369da7b36d0
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-11 17:33:38 -0500

    [margin rectangulo]

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -73,7 +73,8 @@
 		height: 70px;
 		background-color: #dfc14e;
 		border-radius: 0 50px 50px 0;
-		margin-left: -12px !important;
+		margin-left: -14px !important;
+		margin-bottom: 20px!important;
 	}
 
 	#colmder {

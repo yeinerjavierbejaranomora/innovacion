84f3b8cf8fe49388d5fa5943118a123096d1d638
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-11 14:07:34 -0500

    form

--- resources/views/login/prueba.blade.php
+++ resources/views/login/prueba.blade.php
@@ -11,7 +11,29 @@ body {
 
 </style>
 
+<div class="container">
+	<div class="row ">
+		<div class="col">
 
+		</div>
+		<div class="col">
+			<h2 class="fw-light"> Sistema de proyeccion Ibero</h2>
+		
+			<form action="#">
+				<div class="mb-4">
+					<label for="email" class="form-label"> Email</label>
+					<input type="email" class= "form-control" name="email">
+				</div>
+				<div class="mb-4">
+					<label for="password" class="form-label"> Contraseña</label>
+					<input type="password" class= "form-control" name="password">
+				</div>
+			</form>
+		
+		
+		</div>
+	</div>
+</div>
 
 
 
--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -49,7 +49,7 @@
                 <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
             </div>
 
-            @if(empty($datos['programas']))
+            @if(count($datos['programas']) === 0)
             <h3 class="text-center">NO HAY DATOS POR MOSTRAR</h3>
 
             <!-- Content Row -->

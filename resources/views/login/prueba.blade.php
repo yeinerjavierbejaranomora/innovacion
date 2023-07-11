226b7639f5470d0300b22dd07dfc02e551b812aa
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-11 14:43:17 -0500

    align

--- resources/views/login/prueba.blade.php
+++ resources/views/login/prueba.blade.php
@@ -3,47 +3,53 @@
 @section('content')
 
 <style>
+	body {
+		background-color: #4a4848;
+		font-family: Montserrat;
+	}
 
-body {
-  background-color: #4a4848;
-  font-family: Montserrat;
-}
+	.bg {
+		background-image: url('public/assets/images/Escudo.png');
+		background-position: center center;
+	}
 
-.bg{
-	background-image: url('public/assets/images/Escudo.png');
-	background-position: center center;
-}
-
+	.abs-center {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		min-height: 100vh;
+	}
 </style>
 
 <div class="container w-60 align-middle shadow" style="background-color:white; border-radius: 25px;">
-	<div class="row">
-		<div class="col" style="background:#dfc14e;border-top-left-radius: 25px 25px; border-bottom-left-radius: 25px 25px; ">
-		<div class="align-content-center">
-			<img src="public/assets/images/Escudo.png" width="300"alt="">
-		</div>
-		</div>
-		<div class="col">
-			<h2 class="fw-light"> Sistema de proyeccion Ibero</h2>
-		
-			<form action="#">
-				<div class="mb-4">
-					<label for="email" class="form-label"> Email</label>
-					<input type="email" class= "form-control" name="email">
+	<div class="abs-center">
+		<div class="row">
+			<div class="col" style="background:#dfc14e;border-top-left-radius: 25px 25px; border-bottom-left-radius: 25px 25px; ">
+				<div class="align-content-center">
+					<img src="public/assets/images/Escudo.png" width="300" alt="">
 				</div>
-				<div class="mb-4">
-					<label for="password" class="form-label"> Contraseña</label>
-					<input type="password" class= "form-control" name="password">
-				</div>
-				<div>
-					<button type="submit" class="btn btn-warning text-white"> Iniciar sesión</button>
-				</div>
-				<div class="my-10">
-					<span> <u><a href="#">Recuperar Contraseña</a></u></span>
-				</div>
-			</form>
-		
-		
+			</div>
+			<div class="col">
+				<h2 class="fw-light"> Sistema de proyeccion Ibero</h2>
+
+				<form action="#">
+					<div class="mb-4">
+						<label for="email" class="form-label"> Email</label>
+						<input type="email" class="form-control" name="email">
+					</div>
+					<div class="mb-4">
+						<label for="password" class="form-label"> Contraseña</label>
+						<input type="password" class="form-control" name="password">
+					</div>
+					<div>
+						<button type="submit" class="btn btn-warning text-white"> Iniciar sesión</button>
+					</div>
+					<div class="my-10">
+						<span> <u><a href="#">Recuperar Contraseña</a></u></span>
+					</div>
+				</form>
+
+			</div>
 		</div>
 	</div>
 </div>

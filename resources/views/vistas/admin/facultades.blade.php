5264e3e8a633d685d4996f0dc191b6ce536b8206
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-11 08:41:53 -0500

    Vista facultades

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -49,7 +49,7 @@
                 <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
             </div>
 
-            <?php var_dump($datos); ?>
+            
             @if(!$datos['programas'])
             <h3 class="text-center">NO HAY DATOS POR MOSTRAR</h3>    
             @endif

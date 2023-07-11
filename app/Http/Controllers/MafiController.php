6914a3172b9bcc07dc2b4da68d5830e9c0fc6a41
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-11 16:21:21 -0500

    [modificacion metodo prerequisitos]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -290,6 +290,7 @@ class MafiController extends Controller
                     $estaPlaneacion = DB::table('planeacion')->select('codMateria')->whereIn('codMateria',$prerequisitos)->where('codBanner','=',$codBanner)->get();
                     $estaPorVer = DB::table('materiasPorVer')->select('codMateria')->whereIn('codMateria',$prerequisitos)->where('codBanner','=',$codBanner)->orderBy('id','ASC')->get();
                     dd($numeroCreditos);
+                    
                     if($estaPlaneacion == '' && $estaPorVer == '' && $numeroCreditos<$numeroCreditosPermitidos):
                         $numeroCreditos = $numeroCreditos + $creditos;
                         $insertPlaneacion = DB::table('planeacion')->insert([

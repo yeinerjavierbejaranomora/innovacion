2e9e8b8f18e8218ae9a3ca365e47551fdc80878a
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-11 15:08:57 -0500

    alineacion

--- resources/views/login/prueba.blade.php
+++ resources/views/login/prueba.blade.php
@@ -6,43 +6,44 @@
 	body {
 		background-color: #4a4848;
 		font-family: Montserrat;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
-
-	
 </style>
 
 <div class="container w-75 align-middle shadow align-items-center" style="background-color:white; border-radius: 25px;">
-	
-		<div class="row">
-			<div class="col text-center" style="background:#dfc14e;border-top-left-radius: 25px 25px; border-bottom-left-radius: 25px 25px; ">
-				<div class="align-content-center">
-					<img src="public/assets/images/Escudo.png" width="300" alt="">
-				</div>
-			</div>
-			<div class="col">
-				<h2 class="fw-light text-center"> Sistema de proyeccion Ibero</h2>
 
-				<form action="#">
-					<div class="mb-4">
-						<label for="email" class="form-label"> Email</label>
-						<input type="email" class="form-control" name="email">
-					</div>
-					<div class="mb-4">
-						<label for="password" class="form-label"> Contraseña</label>
-						<input type="password" class="form-control" name="password">
-					</div>
-					<div class="text-center">
-						<button type="submit" class="btn btn-warning fw-bold text-white"> Iniciar sesión</button>
-					</div>
-					<br>
-					<br>
-					<div class="my-10 text-center">
-						<span> <u><a href="#">Recuperar Contraseña</a></u></span>
-					</div>
-				</form>
-
+	<div class="row">
+		<div class="col text-center" style="background:#dfc14e;border-top-left-radius: 25px 25px; border-bottom-left-radius: 25px 25px; ">
+			<div class="align-content-center">
+				<img src="public/assets/images/Escudo.png" width="300" alt="">
 			</div>
 		</div>
+		<div class="col">
+			<h2 class="fw-light text-center"> Sistema de proyeccion Ibero</h2>
+
+			<form action="#">
+				<div class="mb-4">
+					<label for="email" class="form-label"> Email</label>
+					<input type="email" class="form-control" name="email">
+				</div>
+				<div class="mb-4">
+					<label for="password" class="form-label"> Contraseña</label>
+					<input type="password" class="form-control" name="password">
+				</div>
+				<div class="text-center">
+					<button type="submit" class="btn btn-warning fw-bold text-white"> Iniciar sesión</button>
+				</div>
+				<br>
+				<br>
+				<div class="my-10 text-center">
+					<span> <u><a href="#">Recuperar Contraseña</a></u></span>
+				</div>
+			</form>
+
+		</div>
+	</div>
 
 </div>
 

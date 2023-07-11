dc4a54dc8dc651e8c28d08d1a46628a865db2f9e
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-11 13:02:25 -0500

    [vista]

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -53,7 +53,7 @@
 
             <!-- Content Row -->
             <div class="row" id="facultades">
-                @if(!isset($datos['programas']))
+                @if(empty($datos['programas']))
                 <h3 class="text-center">NO HAY DATOS POR MOSTRAR</h3>
                 @endif
                 @foreach($datos['programas'] as $key => $value)

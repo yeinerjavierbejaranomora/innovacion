6eb7b82892a0b1e6f0002da2a1b0fd995b469951
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-12 15:21:25 -0500

    styles

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -23,6 +23,7 @@
         background-color: #4a4a48;
         border-radius: 30px;
         color: white;
+        cursor: pointer;
     }
 
     .inactivo i {
@@ -74,8 +75,8 @@
                 @foreach($estudiantes as $key => $est)
                 @if($value->codprograma == $key)
                 <!-- Earnings (Monthly) Card Example -->
-                <div class="col-xl-3 col-md-6 mb-4 inactivo">
-                    <button id="mostrar" name="mostrar" type="input" value="{{ $value->id }}" class="mostrar btn button">
+                <div class="col-xl-3 col-md-6 mb-4 inactivo mostrar" value="{{ $value->id }}">
+                   
                     <div class="card shadow h-100 py-2 text-white">
                         <div class=" card-body">
                             <div class="row text-center">
@@ -97,7 +98,7 @@
                                 </div>
                             </div>
                         </div>
-                    </button>
+        
                     </div>
                     @endif
                 @endforeach

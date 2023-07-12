b10a8482a7a7f49ccbe26f8def075cb397761dd8
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-12 15:46:41 -0500

    Styles

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -80,7 +80,7 @@
                 @if($value->codprograma == $key)
                 <!-- Earnings (Monthly) Card Example -->
                 <div class="col-xl-3 col-md-6 mb-4" >
-                    <div class="card shadow h-100 py-2 inactivo mostrar" type="submit" value="{{ $value->id }}">
+                    <div class="card shadow h-100 py-2 inactivo mostrar programas" type="submit" value="{{ $value->id }}">
                         <div class=" card-body ">
                             <div class="row text-center">
                                 <div class="col mx-auto">
@@ -161,8 +161,8 @@
         $(document).ready(function() {
             var id = null;
             $(document).on("click", ".mostrar", function() {
-                $(".card").removeClass("activo");
-                $(".card").addClass("inactivo");
+                $(".programas").removeClass("activo");
+                $(".programas").addClass("inactivo");
                 $(this).removeClass("inactivo");
                 $(this).addClass("activo");
 

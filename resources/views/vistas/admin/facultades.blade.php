19d9d3c802789deafb5191ad1000049bc60c11eb
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-12 16:08:29 -0500

    [vista]

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -110,9 +110,9 @@
             <!--Nav Datos de la Facultad-->
   
                 <nav class="nav nav-pills nav-justified" id="nav" name="nav" style="display: none;">
-                    <a class="nav-link active" href="#estudiantes">Estudiantes</a>
-                    <a class="nav-link" href="#malla">Malla Curricular</a>
-                    <a class="nav-link" href="#proyecciones">Proyecciones</a>
+                    <a class="nav-link active" href="#estudiantes"><h5>Estudiantes</h5></a>
+                    <a class="nav-link" href="#malla"><h5>Malla Curricular</h5></a>
+                    <a class="nav-link" href="#proyecciones"><h5>Proyecciones</h5></a>
                 </nav>
 
             <br>

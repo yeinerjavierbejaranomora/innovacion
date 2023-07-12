84f5ba52e6aabdd731652ec3be56a8e220393dce
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-12 10:53:23 -0500

    [modificacion metodo materias por ver programar primer ciclo]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -255,7 +255,7 @@ class MafiController extends Controller
                 $ciclo = $materia->ciclo;
                 $prerequisitosConsulta = $this->prerequisitos($codMateria,$programa);
                 $prerequisitos = [$prerequisitosConsulta->prerequisito];
-                if($prerequisitos=="" && $ciclo!=2 && $cuentaCursosCiclo1<$numeroMateriasPermitidos):
+                if($prerequisitos=='' && $ciclo!=2 && $cuentaCursosCiclo1<$numeroMateriasPermitidos):
                     var_dump($prerequisitos,"--",$ciclo,'---',$cuentaCursosCiclo1,'----','sin P','<br>');
                 else:
                     var_dump($prerequisitos,"--",$ciclo,'---',$cuentaCursosCiclo1,'----','con P','<br>');

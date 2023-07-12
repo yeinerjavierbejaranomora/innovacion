1d8118a671b964cbabafb9a3c8fe409925dfea22
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-07-12 11:59:02 -0500

    [modificacion metodo materias por ver programar primer ciclo]

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -256,10 +256,10 @@ class MafiController extends Controller
                 $prerequisitosConsulta = $this->prerequisitos($codMateria,$programa);
                 $prerequisitos = $prerequisitosConsulta->prerequisito;
                 //dd($prerequisitos);
-                //dd($numeroCreditos);
                 if($prerequisitos=='' && $ciclo!=2 && $cuentaCursosCiclo1<$numeroMateriasPermitidos):
                     /**SELECT codMateria FROM planeacion WHERE codMateria="'.$codMateria.'" AND  	codBanner="'.$codBanner.'"; */
                     $estaPlaneacion = DB::table('planeacion')->select('codMateria')->where([['codMateria','=',$codMateria],['codBanner','=',$codBanner]])->dd();
+                    dd($numeroCreditos);
 
                     if($estaPlaneacion->codMateria == '' && $numeroCreditos<$numeroCreditosPermitidos):
 

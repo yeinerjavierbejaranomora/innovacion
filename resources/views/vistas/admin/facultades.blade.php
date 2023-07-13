cdab321bc113b18a49f23976729e9ee1bdf286f1
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-13 11:10:12 -0500

    [text center]

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -75,7 +75,7 @@
         <div class="container-fluid">
 
             <!-- Page Heading -->
-            <div class="d-sm-flex align-items-center text-center mb-4">
+            <div class="text-center mb-4">
                 <h1 class="h3 mb-0 text-gray-800 text-center" style="font-weight: 100;">Programas facultad de {{$datos['facultad']}}</h1>
                 <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
             </div>

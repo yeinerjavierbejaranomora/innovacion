d20dc96cb8f28fcee9b5a499a3b7e0dd30812d2f
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-14 15:31:43 -0500

    [Documentación]

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
+require_once '../phpDocumentor.phar';
 
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)

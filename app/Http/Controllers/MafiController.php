463d86d5de14da6aa8d5ccb1a3690cd5d7e6aa1b
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-07-13 21:02:46 -0500

    1

--- app/Http/Controllers/MafiController.php
+++ app/Http/Controllers/MafiController.php
@@ -247,7 +247,7 @@ class MafiController extends Controller
                                     ->where('planeacion.codBanner', '=', $codigoBanner)
                                     ->where('mallaCurricular.codprograma','=','planeacion.codprograma')
                                     ->groupBy('planeacion.codBanner')
-                                    ->dd();
+                                    ->first();
             dd($codigoBanner,$creditosPlaneados);
         endforeach;
         die();

7c3d81d4fcd0b1e2a1dcbf9187e92813dab182fe
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-17 10:58:49 -0500

    test

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -794,7 +794,7 @@ class facultadController extends Controller
         $sello = DB::table('datosMafiReplica')
             ->join('programas', 'programas.programa', '=', 'datosMafiReplica.codprograma')
             // ->join('facultad', 'facultad.id', '=', 'programas.idFacultad')
-            ->where('facultad.id', $id)
+            // ->where('facultad.id', $id)
             ->where('programas.activo', 1)
             ->select('datosMafiReplica.sello', DB::raw('COUNT(datosMafiReplica.sello) AS TOTAL'))
             ->groupBy('datosMafiReplica.sello')

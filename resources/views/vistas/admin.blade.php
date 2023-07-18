986273cca3975b83312a54449e1c7a31af0075f9
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-18 14:15:06 -0500

    prueba

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -18,7 +18,7 @@
     }
 </style>
 
-
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 <!-- Content Wrapper -->
 <div id="content-wrapper" class="d-flex flex-column">
 

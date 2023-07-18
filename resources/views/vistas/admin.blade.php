c07c4b3377dbda4920077ee734faa503d0bf7fec
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-18 13:07:22 -0500

    dbug

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -123,6 +123,7 @@
                     contentType: false,
                     processData: false,
                     success: function(data) {
+                        console.log(data);
                         data.data.forEach(programa => {
                             $('#programas').append(`<label><input type="checkbox" id="" name="programa[]" value="${programa.id}"> ${programa.nombre}</label><br>`);
                         });

698c39b5964d5c3feac76e0b2dc9c4be8a5fb1b0
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-18 14:43:35 -0500

    []

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -21,9 +21,6 @@
         max-height: 320px;
     }
     
-    .card-body {
-        overflow: auto;
-    }
 </style>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>

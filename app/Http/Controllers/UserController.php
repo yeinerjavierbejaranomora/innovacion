e623cbdead27f020fee7c75293e28299da419f7f
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-18 11:56:50 -0500

    [consulta]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -503,8 +503,7 @@ class UserController extends Controller
     public function traerProgramas()
     {
         $idsFacultad = $_POST;
-        $programas = DB::table('programs')->whereIn('id',$idsFacultad)->select('id','programa')
-        ->get();
+        $programas = DB::table('programas')->whereIn('id',$idsFacultad)->select('id','programa')->get();
         header("Content-Type: application/json");
         echo json_encode(array('data' => $programas));
     }

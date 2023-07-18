8e69a24c936ce6b2b45072a81cebdcbfcb804abe
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-07-18 14:19:13 -0500

    test

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -123,7 +123,7 @@
                     contentType: false,
                     processData: false,
                     success: function(datos) {
-
+                        datos=json_decode(datos);
                         console.log('cambio');
                         console.log(datos);
                         datos.each(programa => {

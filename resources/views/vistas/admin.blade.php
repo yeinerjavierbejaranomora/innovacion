49f8838c95addbfb0a79307d1bd8c16de9e204c6
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-07-19 12:33:35 -0500

    test

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -367,7 +367,7 @@
                         return elemento.autorizado_asistir;
                     });
                     var valores = data.data.map(function(elemento) {
-                        elemento.TOTAL.push(total);
+                       
                         return elemento.TOTAL;
                     });
 

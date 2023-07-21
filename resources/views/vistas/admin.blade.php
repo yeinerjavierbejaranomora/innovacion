c2f1f669aa156a7e64e6d33c43039549071cf122
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-21 16:36:28 -0500

    [colores]

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -840,7 +840,7 @@
                             datasets: [{
                                 label: 'Gráfico Circular',
                                 data: valores,
-                                backgroundColor: ['rgba(223, 193, 78, 1)', 'rgba(74, 72, 72, 1)']
+                                backgroundColor: ['rgba(223, 193, 78, 1)', 'rgba(74, 72, 72, 1)', 'rgba(56,101,120,1)']
                             }]
                         },
                         options: {

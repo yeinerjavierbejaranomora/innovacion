5e40d2bea474fde5776347e4fdbc1fd1b2462e93
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-25 14:23:11 -0500

    test

--- resources/views/vistas/admin.blade.php
+++ resources/views/vistas/admin.blade.php
@@ -95,11 +95,11 @@
                             <h5><strong>Seleccionar Facultades</strong></h5>
                         </div>
                         <div class="card-body text-start" id="centrar" style="overflow: auto;">
+                            <div class="facultades" name="facultades" id="facultades" style="display: none;">
                             <div class="form-check">
                                 <label> <input type="checkbox" class="form-check-input" value="todos" id="mostrarTodos" checked> Ver Todo</label>
                             </div>
                             <br>
-                            <div class="facultades" name="facultades" id="facultades" style="display: none;">
                             </div>
                         </div>
                     </div>

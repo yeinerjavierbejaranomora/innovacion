78efe6ab12b9f647e066321986769b38e40e83ff
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-27 09:28:02 -0500

    [Traer programas]

--- resources/views/vistas/admin/facultades.blade.php
+++ resources/views/vistas/admin/facultades.blade.php
@@ -300,7 +300,7 @@
         function traerProgramas();
         function traerProgramas() {
             var formData = new FormData();
-            formData =['idfacultad[]', "<?=$nombre?>"];
+            formData =('idfacultad[]', "<?=$nombre?>");
             $.ajax({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')

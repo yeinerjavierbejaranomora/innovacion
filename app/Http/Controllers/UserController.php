af1b150c5721dcddc324d0a51454630c256d31e2
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-28 12:50:52 -0500

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -99,7 +99,7 @@ class UserController extends Controller
             $facultades = explode(",", $idfacultad);
                 foreach ($facultades as $key => $value) {
                 $consulta = DB::table('users as u')->join('facultad as f', 'f.id', '=', 'u.id_facultad')->where('f.id',$value)->select('f.nombre as name')->get();
-                $nombreFacultades[$value] = $consulta->name;
+                $nombreFacultades[$value] = $consulta;
                 }
             return view('vistas.Decano', ['facultades' => $nombreFacultades])->with('datos', $datos);
         }

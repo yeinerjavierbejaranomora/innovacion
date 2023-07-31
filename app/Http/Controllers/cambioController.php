92b9858b8ece2fd2fa69e45da2a679208e7fea00
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-30 20:01:30 -0500

    test

--- app/Http/Controllers/cambioController.php
+++ app/Http/Controllers/cambioController.php
@@ -84,7 +84,7 @@ class cambioController extends Controller
 
         if($user->id_facultad)
         {
-            $facultad = DB::table('facultad')->where([['id', '=', $user->id_facultad]])->get();
+            $facultad = DB::table('facultad')->where([['id', '=', $user->id_facultad]])->select('nombre')->get();
         }
         else
         {

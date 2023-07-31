e5ec81c8905970ed7c475fd68b252b764c9ae545
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-07-31 15:23:22 -0500

    [estado de usuario]

--- resources/views/vistas/Coordinador.blade.php
+++ resources/views/vistas/Coordinador.blade.php
@@ -308,11 +308,11 @@
             $("#mensaje").empty();
             if (programasSeleccionados.length > 1)
             {
-                var textoNuevo = "<h3>Informe programas" + programasSeleccionados +"</h3>";
+                var textoNuevo = "<h3>Informe programas " + programasSeleccionados +" </h3>";
             }
             else
             {
-                var textoNuevo = "<h3>Informe programa" + programasSeleccionados +"</h3>";
+                var textoNuevo = "<h3>Informe programa " + programasSeleccionados +" </h3>";
             }
              $("#mensaje").html(textoNuevo);
         }

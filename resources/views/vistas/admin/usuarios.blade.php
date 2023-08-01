810cbc353eab41ef400a2344396ec8277e41b19b
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-01 12:15:53 -0500

    test

--- resources/views/vistas/admin/usuarios.blade.php
+++ resources/views/vistas/admin/usuarios.blade.php
@@ -203,7 +203,7 @@
                     }
                     console.log(datos); 
                     $.each(datos, function(key, value) {
-                        console.log(value);
+                        console.log(value.id);
                         $('#programas').append(`<label><input type="checkbox" id="" name="programa[]" value="${value.id}"> ${value.nombre}</label><br>`);
                     });
                 }

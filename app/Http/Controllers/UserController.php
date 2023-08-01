49bb7593991931c9606634fd0db7dce87243b2fc
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-01 12:06:49 -0500

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -517,7 +517,7 @@ class UserController extends Controller
         $nombreFacultad = DB::table('facultad')->whereIn('codfacultad',$codFacultad)->select('nombre')->get();
         $programasPorFacultad = array();
         foreach ($nombreFacultad as $facultad){
-            $programas = DB::table('programas')->whereIn('Facultad', $facultad)->select('id', 'programa', 'codprograma')->get();
+            $programas = DB::table('programas')->whereIn('Facultad', $facultad->nombre)->select('id', 'programa', 'codprograma')->get();
             $programasPorFacultad[$facultad->nombre] = $programas;   
         }
         var_dump($programasPorFacultad);

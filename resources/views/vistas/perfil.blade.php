5ed972d840ec575bbef210450181946280237407
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-02 15:30:47 -0500

    [Editar contraseña]

--- resources/views/vistas/perfil.blade.php
+++ resources/views/vistas/perfil.blade.php
@@ -287,7 +287,7 @@
                                 </div>
                             </div>
                             <div class="text-center">
-                                <a class="" href="{{ route('cambio.cambio') }}" role="button"><u>Cambiar Contraseña</u></a>
+                                <a class="" href="{{ route('cambio.cambio', ['idbanner' => encrypt(auth()->user()->id_banner)]) }}" role="button"><u>Cambiar Contraseña</u></a>
                             </div>
                         </div>
                     </div>

de09942bb640bd757d727f56302aa9e6dc3bbab5
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-03 15:38:38 -0500

    [hidden

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -189,7 +189,7 @@
                     <div class="card shadow mb-5 graficos" id="chartEstudiantes">
                         <div class="card-header">
                             <h5 id="tituloEstudiantes"><strong>Total estudiantes Banner</strong></h5>
-                            <h5 class="tituloPeriodo"><strong> Periodo</strong></h5>
+                            <h5 class="tituloPeriodo hidden"><strong>Periodo</strong></h5>
                         </div>
                         <div class="card-body">
                             <canvas id="estudiantes"></canvas>
@@ -200,7 +200,7 @@
                 <div class="card shadow mb-6 graficos">
                     <div class="card-header">
                         <h5 id="tituloEstadoFinanciero"><strong>Estado Financiero</strong></h5>
-                        <h5 class="tituloPeriodo"><strong> Periodo</strong></h5>
+                        <h5 class="tituloPeriodo hidden"><strong> Periodo</strong></h5>
                     </div>
                     <div class="card-body">
                         <canvas id="activos"></canvas>
@@ -211,7 +211,7 @@
                 <div class="card shadow mb-6 graficos">
                     <div class="card-header">
                         <h5 id="tituloRetencion"><strong>Estado Financiero - Retención</strong></h5>
-                        <h5 class="tituloPeriodo"><strong> Periodo</strong></h5>
+                        <h5 class="tituloPeriodo hidden"><strong> Periodo</strong></h5>
                     </div>
                     <div class="card-body">
                         <canvas id="retencion"></canvas>
@@ -222,7 +222,7 @@
                 <div class="card shadow mb-6 graficos">
                     <div class="card-header">
                         <h5 id="tituloEstudiantesNuevos"><strong>Estudiantes nuevos - Estado Financiero</strong></h5>
-                        <h5 class="tituloPeriodo"><strong> Periodo</strong></h5>
+                        <h5 class="tituloPeriodo hidden"><strong> Periodo</strong></h5>
                     </div>
                     <div class="card-body">
                         <canvas id="primerIngreso"></canvas>
@@ -233,7 +233,7 @@
                 <div class="card shadow mb-6 graficos">
                     <div class="card-header">
                         <h5 id="tituloTipos"><strong>Tipos de estudiantes</strong></h5>
-                        <h5 class="tituloPeriodo"><strong> Periodo</strong></h5>
+                        <h5 class="tituloPeriodo hidden"><strong> Periodo</strong></h5>
                     </div>
                     <div class="card-body">
                         <canvas id="tipoEstudiante"></canvas>
@@ -247,7 +247,7 @@
                 <div class="card shadow mb-6 graficos">
                     <div class="card-header">
                         <h5 id="tituloOperadores"><strong>Operadores</strong></h5>
-                        <h5 class="tituloPeriodo"><strong> Periodo</strong></h5>
+                        <h5 class="tituloPeriodo hidden"><strong>Periodo</strong></h5>
                     </div>
                     <div class="card-body">
                         <canvas id="operadores" style="height: 400px;"></canvas>
@@ -261,7 +261,7 @@
                 <div class="card shadow mb-4 graficos" id="ocultarGraficoProgramas">
                     <div class="card-header">
                         <h5 id="tituloProgramas"><strong>Programas con mayor cantidad de admitidos</strong></h5>
-                        <h5 class="tituloPeriodo"><strong> Periodo</strong></h5>
+                        <h5 class="tituloPeriodo hidden"><strong>Periodo</strong></h5>
                     </div>
                     <div class="card-body">
                         <canvas id="estudiantesProgramas"></canvas>

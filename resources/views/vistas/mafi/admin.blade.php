28b6ec2dc29e3649f24e6f5c2fc77eaba4d6b5f9
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-03 14:55:03 -0500

    test

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -106,8 +106,9 @@
                 <div class="input-group-append">
                     <h3> Bienvenido {{auth()->user()->nombre}}</h3>
                 </div>
+                -
                 <div class="text-center">
-                <h1 class="h3 mb-0 text-gray-800"> <strong>Informe de Facultades</strong></h1>
+                <h1 class="h3 mb-0 text-gray-800"> <strong> Informe de Facultades</strong></h1>
                  </div>
             </div>
 

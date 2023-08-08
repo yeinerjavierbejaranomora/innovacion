c275109b1f710fae3d6858ad24587112046a6d2d
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-08 12:39:33 -0500

    test

--- resources/views/vistas/planeacion/DirectorPlaneacion.blade.php
+++ resources/views/vistas/planeacion/DirectorPlaneacion.blade.php
@@ -456,7 +456,7 @@
 
         $('#generarReporte').on('click', function(e) {
             e.preventDefault();
-            contador();
+            Contador();
             if ($('#programas input[type="checkbox"]:checked').length > 0) {
                 if ($('#programas input[type="checkbox"]:checked').length == totalSeleccionado && $('#periodos input[type="checkbox"]:checked').lenght == totalPeriodos) {
                     location.reload();

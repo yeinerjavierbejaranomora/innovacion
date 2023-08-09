ba3142271a9f7592352096b04c75288eba254d2d
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-09 12:36:11 -0500

    titulo

--- resources/views/vistas/moodle/adminMoodle.blade.php
+++ resources/views/vistas/moodle/adminMoodle.blade.php
@@ -454,26 +454,6 @@
         function grafico() {
             var ctx = document.getElementById('alto').getContext('2d');
 
-            Chart.pluginService.register({
-                beforeDraw: function(chart) {
-                    var width = chart.chart.width,
-                        height = chart.chart.height,
-                        ctx = chart.chart.ctx;
-
-                    ctx.restore();
-                    var fontSize = (height / 100).toFixed(2);
-                    ctx.font = fontSize + "em sans-serif";
-                    ctx.textBaseline = "middle";
-
-                    var text = titulo,
-                        textX = Math.round((width - ctx.measureText(text).width) / 2),
-                        textY = height / 2;
-
-                    ctx.fillText(text, textX, textY);
-                    ctx.save();
-                }
-            });
-
             chartRiesgo = new Chart(ctx, {
                 type: 'doughnut',
                 data: {
@@ -498,16 +478,14 @@
                         title: {
                             display: true,
                             text: 'titulo',
+                            positon: 'bottom',
+                            size: '14',
                         },
                         tooltip: {
                             enabled: false
                         }
                     },
-                    layout: {
-                        padding: {
-                            top: 20,
-                        }
-                    }
+
                 },
                 plugins: [ChartDataLabels]
             });

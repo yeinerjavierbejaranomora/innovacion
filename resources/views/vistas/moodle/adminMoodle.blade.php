aee20a37ef65d1b275c3e2d7c4e650cafd5d83d7
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-09 12:34:55 -0500

    [test]

--- resources/views/vistas/moodle/adminMoodle.blade.php
+++ resources/views/vistas/moodle/adminMoodle.blade.php
@@ -453,6 +453,27 @@
 
         function grafico() {
             var ctx = document.getElementById('alto').getContext('2d');
+
+            Chart.pluginService.register({
+                beforeDraw: function(chart) {
+                    var width = chart.chart.width,
+                        height = chart.chart.height,
+                        ctx = chart.chart.ctx;
+
+                    ctx.restore();
+                    var fontSize = (height / 100).toFixed(2);
+                    ctx.font = fontSize + "em sans-serif";
+                    ctx.textBaseline = "middle";
+
+                    var text = titulo,
+                        textX = Math.round((width - ctx.measureText(text).width) / 2),
+                        textY = height / 2;
+
+                    ctx.fillText(text, textX, textY);
+                    ctx.save();
+                }
+            });
+
             chartRiesgo = new Chart(ctx, {
                 type: 'doughnut',
                 data: {

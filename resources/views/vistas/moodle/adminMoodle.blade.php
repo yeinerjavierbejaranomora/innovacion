2eae630a95df32225f7468996bbceaf7f10a8f50
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-10 10:10:51 -0500

    debug

--- resources/views/vistas/moodle/adminMoodle.blade.php
+++ resources/views/vistas/moodle/adminMoodle.blade.php
@@ -625,6 +625,7 @@
                         function obtenerData(tbody, table) {
                             $(tbody).on("click", "button.data", function() {
                                 var data = table.row($(this).parents("tr")).data();
+                                console.log (data);
                                 dataAlumno(data.Id_Banner);
                             })
                         }

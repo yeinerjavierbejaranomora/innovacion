d630b117ae43989e66ae39b4f1933fbe0713b418
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-15 09:49:09 -0500

    [debug]

--- resources/views/vistas/moodle/adminMoodle.blade.php
+++ resources/views/vistas/moodle/adminMoodle.blade.php
@@ -673,16 +673,19 @@
 
                         if (TotalAlto <= 0)
                         {
-                            TotalAlto = 0;
+                            TotalAlto == 0;
                         }
                         if (TotalMedio <= 0)
                         {
-                            TotalMedio = 0;
+                            TotalMedio == 0;
                         }
                         if (TotalBajo <= 0)
                         {
-                            TotalBajo = 0;
+                            TotalBajo == 0;
                         }
+                        console.log(TotalAlto);
+                        console.log(TotalMedio);
+                        console.log(TotalBajo);
                         chartRiesgoAlto = new Chart(ctx, {
                             type: 'doughnut',
                             data: {

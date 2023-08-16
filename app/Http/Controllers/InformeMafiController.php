d5fe8c40a7fe2f36f492e774d5fb6beb12ec3fee
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-08-16 16:13:03 -0500

    test

--- app/Http/Controllers/InformeMafiController.php
+++ app/Http/Controllers/InformeMafiController.php
@@ -1220,7 +1220,7 @@ class InformeMafiController extends Controller
         
          $periodos = DB::table('periodo')->get();
 
-         $fcontinua=array(04,05,06,07,08);
+         $fcontinua=array(4,5,6,7,8);
          $pregrado=array(11,12,13,16,17,31,32,33,34,35);
          $especializacion=array(41,42,43,44,45);
          $maestria=array(51,52,53,54,55);

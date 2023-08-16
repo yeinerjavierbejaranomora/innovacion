a2cda00919283d50e09ad748664511b1f0b95868
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-08-16 16:40:54 -0500

    test

--- app/Http/Controllers/InformeMafiController.php
+++ app/Http/Controllers/InformeMafiController.php
@@ -1226,7 +1226,7 @@ class InformeMafiController extends Controller
             $maestria=array("51","52","53","54","55");
 
             foreach ( $programas as $key_programas => $val_programas) {
-                dd($val_programas);
+               
                if($val_programas->nivelFormacion=="EDUCACION CONTINUA"){
 
                 foreach ($fcontinua as $key => $value) {

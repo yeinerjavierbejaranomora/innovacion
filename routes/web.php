df0dd33a8aa1b11b0f76f20a369ddc166c788a70
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-17 12:45:36 -0500

    [Ruta]

--- routes/web.php
+++ routes/web.php
@@ -344,9 +344,9 @@ Route::controller(facultadController::class)->group(function () {
     /** Ruta para cargar la vista de planeación*/
     Route::get('/home/programasPeriodos', 'vistaProgramasPeriodos')->middleware('auth', 'admin')->name('programasPeriodos.view');
     /** Ruta para cargar tabla programasPeriodos */
-    Route::post('/home/tablaProgramasPeriodos', 'getProgramasPeriodos')->middleware('auth', 'admin')->name('programasPeriodos.tabla');
+    Route::post('/home/tablaProgramasPeriodos', 'getProgramasPeriodos')->middleware('auth')->name('programasPeriodos.tabla');
     /** Ruta para cargar tabla programasPeriodos por Facultad*/
-    Route::post('/home/tablaProgramasPeriodosFacultad', 'getProgramasPeriodosFacultad')->middleware('auth', 'admin')->name('programasPeriodos.tabla.facultad');
+    Route::post('/home/tablaProgramasPeriodosFacultad', 'getProgramasPeriodosFacultad')->middleware('auth')->name('programasPeriodos.tabla.facultad');
     /** Ruta para inactivar periodo */
     Route::post('/home/inactivarProgramaPeriodo', 'inactivarProgramaPeriodo')->middleware('auth')->name('programasPeriodos.inactivar');
     /** Ruta para activar periodo */

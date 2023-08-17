072800dd3a17103736ea1ce925d6eb7fa35ed65e
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-17 09:35:27 -0500

    accordion

--- resources/views/vistas/admin/programasPeriodos.blade.php
+++ resources/views/vistas/admin/programasPeriodos.blade.php
@@ -147,23 +147,24 @@
                             <h5 class="tituloPeriodo"><strong></strong></h5>
                         </div>
                         <div class="card-body center-chart">
+                            <!--Accordion-->
+                            <div id="accordion">
+                                <div class="card">
+                                    <div class="card-header" id="headingOne">
+                                        <h5 class="mb-0">
+                                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                Pregrado
+                                            </button>
+                                        </h5>
+                                    </div>
 
-                            <div class="card">
-                                <div class="card-header" id="headingOne">
-                                    <h5 class="mb-0">
-                                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            Pregrado
-                                        </button>
-                                    </h5>
-                                </div>
+                                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                                        <div class="card-body">
 
-                                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                                    <div class="card-body">
-
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-
 
                         </div>
                         <div class="card-footer d-flex justify-content-end">
@@ -248,7 +249,12 @@
     @endif
 
     <script>
+
+
+
         $(document).ready(function() {
+
+            $('.collapse').collapse();
 
             var xmlhttp = new XMLHttpRequest();
             var url = "{{ route('programasPeriodos.tabla') }}";

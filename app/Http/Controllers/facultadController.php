c2c2be641d99db89ddc9779ca7acdea0bc81f447
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-17 10:24:29 -0500

    []

--- app/Http/Controllers/facultadController.php
+++ app/Http/Controllers/facultadController.php
@@ -825,11 +825,11 @@ class facultadController extends Controller
      */
     public function programasActivos()
     {
-        $programasActivos = DB::table('programasPeriodos as pP')
-            ->join('programas as p', 'pP.codPrograma', '=', 'p.codprograma')
-            ->select('p.nivelFormacion', 'pP.periodo')
-            ->where('pP.estado', '1')
-            ->groupBy('pP.periodo')
+        $programasActivos = DB::table('programasPeriodos as P')
+            ->join('programas as a', 'P.codPrograma', '=', 'a.codprograma')
+            ->select('a.nivelFormacion', 'P.periodo')
+            ->where('P.estado', '1')
+            ->groupBy('P.periodo')
             ->get();
 
         return $programasActivos;

981d165435300efe395d66a7f94a71844b313213
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-17 09:37:29 -0500

    []

--- resources/views/vistas/admin/programasPeriodos.blade.php
+++ resources/views/vistas/admin/programasPeriodos.blade.php
@@ -150,7 +150,7 @@
                             <!--Accordion-->
                             <div id="accordion">
                                 <div class="card">
-                                    <div class="card-header" id="headingOne">
+                                    <div class="card-header" id="headingOne" style="width:100%;">
                                         <h5 class="mb-0">
                                             <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                                 Pregrado
@@ -159,7 +159,7 @@
                                     </div>
 
                                     <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                                        <div class="card-body">
+                                        <div class="card-body" style="width:100%;">
 
                                         </div>
                                     </div>

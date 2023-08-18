cb5b607d628aa386bc64d67ab3656699643ed4a1
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-18 09:45:57 -0500

    [historial estudiante]

---
+++ app/Http/Controllers/EstudianteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EstudianteController extends Controller
+{
+    //
+
+    public function inicio(){
+        
+    }
+}

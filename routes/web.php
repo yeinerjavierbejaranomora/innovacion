8a42d95c3092500f132a9a1c307058e505e52dd8
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-18 14:55:27 -0500

    [historial estudiante][obtener data programacion]

--- routes/web.php
+++ routes/web.php
@@ -373,7 +373,7 @@ Route::controller(HistorialEstudianteController::class)->group(function () {
 
 Route::controller(EstudianteController::class)->group(function(){
     Route::get('/historialestudiante','inicio')->name('historial.inicio');
-    Route::post('/historialestudiante/consulta','consultaEstudiante')->name('historial.consultaestudiante');
+    Route::post('/historialestudiante/consulta','consultaEstudiante')->name('historial.consulta');
     Route::post('/historialestudiante/consulta','consultaMalla')->name('historial.consultamalla');
 });
 

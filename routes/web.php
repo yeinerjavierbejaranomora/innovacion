c5743de31fcf7868c322901325b688f1dfe0a15e
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-18 11:39:30 -0500

    [Llenar tabla]

--- routes/web.php
+++ routes/web.php
@@ -158,6 +158,8 @@ Route::controller(InformeMafiController::class)->group(function () {
 
     Route::get('/historial_graficos', 'historial_graficos')->middleware('auth', 'admin')->name('historial_graficos');
 
+    Route::get('/home/probar', 'tablaMetas')->middleware('auth', 'admin')->name('funcion.probar');
+
 });
 
 Route::controller(InformeMoodleController::class)->group(function () {

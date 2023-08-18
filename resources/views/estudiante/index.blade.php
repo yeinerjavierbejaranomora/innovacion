9ac42fb924384ed6fa9758898db321fc40466b76
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-18 16:23:38 -0500

    [historial estudiante][maquetar informacion]

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -124,6 +124,7 @@
             <br>
 
             <div class="container-fluid">
+                <div class="container mt-3">
                     <div class="row py-5">
                         <div class="col 4 text-center">
                             <a type="button" class="btn boton" href="{{ route('home.mafi') }}">

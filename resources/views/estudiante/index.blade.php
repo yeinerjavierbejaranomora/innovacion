2cb5d9802a429eecfe7b8c506700369f65719b62
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-18 16:24:41 -0500

    [historial estudiante][maquetar informacion]

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -127,18 +127,18 @@
                 <div class="container mt-3">
                     <div class="row py-5">
                         <div class="col 4 text-center">
-                            <a type="button" class="btn boton" href="{{ route('home.mafi') }}">
-                                Banner
+                            <a type="button" class="btn boton" href="">
+                                Malla curricular
                             </a>
                         </div>
                         <div class="col 4 text-center">
-                            <a type="button" class="btn boton" href="{{ route('home.moodle') }}">
-                                Moodle
+                            <a type="button" class="btn boton" href="">
+                                Historial
                             </a>
                         </div>
                         <div class="col 4 text-center">
-                            <a type="button"class="btn boton" href="{{ route('home.planeacion') }}">
-                                Planeación
+                            <a type="button"class="btn boton" href="">
+                                Programado
                             </a>
                         </div>
                     </div>

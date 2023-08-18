5953273494b9292c2e172447dba3a15bbf25d364
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-08-18 16:54:22 -0500

    [historial estudiante][maquetar informacion]

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -266,10 +266,10 @@
                 contentType: false,
                 processData: false,
                 beforeSend: function(){
-                    //$('#codigo').prop('disabled',true);
+                    $('#codigo').prop('disabled',true);
                 },
                 success: function(data){
-                    //$('#codigo').prop('disabled',false);
+                    $('#codigo').prop('disabled',false);
                     if(data.homologante != ''){
                         $('#botones').html('');
                         $('#botones').append(`<div class="col 4 text-center">
@@ -293,7 +293,7 @@
             });
         }
 
-        /*function consultaMalla(codBanner) {
+        function consultaMalla(codBanner) {
             var formData = new FormData();
             formData.append('codBanner',codBanner);
             $.ajax({
@@ -357,7 +357,7 @@
                     console.log(data);
                 }
             });
-        }*/
+        }
 
     </script>
     @include('layout.footer')

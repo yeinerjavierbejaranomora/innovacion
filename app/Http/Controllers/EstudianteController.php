6c2ed70b8e7e110cec8b48475338ffdd1351f70f
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-22 11:17:20 -0500

    [Vista historial estudiante][datos del estudiante]

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -25,7 +25,7 @@ class EstudianteController extends Controller
         if($consultaNombre != NULL):
             var_dump($consultaNombre);die();
         else:
-            $consultaNombre = DB::table('historialAcademico')->where('codBanner','=',$estudiante)->select('nombreEst')->firts();
+            $consultaNombre = DB::table('historialAcademico')->where('codBanner','=',$estudiante)->select('nombreEst')->first();
             var_dump('vacio');die();
         endif;
         return $consultaNombre;

a9fdfde5d999d3af8cb945a46cbd484c73458f2d
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-22 11:16:20 -0500

    [width]

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -227,7 +227,7 @@
                         </h5>
                     </div>
                     <div class="card-body">
-                        <canvas id="retencion" style="width: 60%;"></canvas>
+                        <canvas id="retencion" style="width: 50%;"></canvas>
                     </div>
                 </div>
             </div>

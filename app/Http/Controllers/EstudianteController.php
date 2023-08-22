36362bc0e2a95e1ed854a0db0440e9b45ca07db0
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-22 14:29:55 -0500

    [Vista historial estudiante][datos del estudiante]

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -23,7 +23,7 @@ class EstudianteController extends Controller
         $estudiante = $_POST['codBanner'];
         $consultaNombre = DB::table('datos_moodle')->where('Id_Banner','=',$estudiante)->select('Nombre','Apellido')->first();
         if($consultaNombre != NULL):
-            var_dump($consultaNombre['Nombre']);die();
+            var_dump($consultaNombre->Nombre);die();
         else:
             $consultaNombre = DB::table('historialAcademico')->where('codBanner','=',$estudiante)->select('nombreEst')->first();
             if($consultaNombre != NULL):

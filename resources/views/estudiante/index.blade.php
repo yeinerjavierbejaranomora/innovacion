cbff0e87a7d1c4c4f79279012bdbb1b277cc4f54
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-22 08:57:43 -0500

    [Vista historial estudiante][malla curricular]

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -139,7 +139,7 @@
             <div class="col-10 text-center" id="colSelloFinanciero">
                 <div class="card shadow mb-6 graficos">
                     <div class="card-header">
-                        <h5 class="titulos"><strong>Total estudiantes con sello financiero</strong></h5>
+                        <h5 class="titulos"><strong>Malla Curricular</strong></h5>
                         <h5 class="facultadtitulos" style="display: none;"><strong>Sello finaciero por Facultad</strong></h5>
                         <h5 class="programastitulos" style="display: none;"><strong>Sello finaciero por Programa</strong></h5>
                     </div>

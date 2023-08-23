e8e4b375c58205672274346d08726b7faa5dbef1
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-23 10:11:51 -0500

    [Vista historial estudiante][datos del estudiante]

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -42,7 +42,7 @@ class EstudianteController extends Controller
     }
 
     public function consultaEstudiante(Request $request){
-        var_dump($request);die();
+        dd($request);
         /*$estudiante = $_POST['codBanner'];
         $consultaEstudiante = DB::table('estudiantes')->where('homologante','=',$estudiante)->first();
         return $consultaEstudiante;*/

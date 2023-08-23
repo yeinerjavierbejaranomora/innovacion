c18039dee32041f81cb95e2d650bc17a64cfeafd
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-23 11:42:17 -0500

    [Vista historial estudiante][datos del estudiante]

--- resources/views/estudiante/historial.blade.php
+++ resources/views/estudiante/historial.blade.php
@@ -102,6 +102,9 @@
             <div class="container mt-3">
                 <div class="row py-5" id="">
                     <?php var_dump(count($programas)); ?>
+                    @for($i = 0; $i < count($programas); $i++)
+                        {{ $programas[$i] }}
+                    @endfor
                     <div class="col 4 text-center">
                         <a type="button" class="btn boton" onclick="consultaMalla({{ $estudiante->homologante}});">
                             Malla curricular

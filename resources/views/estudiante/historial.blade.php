749cb49a6f83ddbc0c1b13a11d8ddd009bf33792
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-23 14:14:12 -0500

    [Vista historial estudiante][datos del estudiante][programas]

--- resources/views/estudiante/historial.blade.php
+++ resources/views/estudiante/historial.blade.php
@@ -101,14 +101,14 @@
         <div class="col-md-10">
             <ul class="nav nav-tabs taps_programas" role="tablist">
                 <?php var_dump($programas);?>
-                @for($i = 0; $i < count($programas); $i++)
+                {{-- @for($i = 0; $i < count($programas); $i++)
                 <?php var_dump($programas['cod_programa']);?>
-                    {{-- @if($i == 0)
+                    @if($i == 0)
                         <li class="nav-item active"><a class="nav-link active" data-toggle="pill" href="#tap_0" role="tab" aria-controls="pills-contact" aria-selected="false">DIP SEG Y SALUD TRA RIES P VIR</a></li>
                     @else
                         <li class="nav-item "><a class="nav-link " data-toggle="pill" href="#tap_1" role="tab" aria-controls="pills-contact" aria-selected="false">ESP EN ANALI Y BIG DATA VIR</a></li>
-                    @endif --}}
-                @endfor
+                    @endif
+                @endfor --}}
             </ul>
         </div>
 

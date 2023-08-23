57eea3c8598f6d895856332f734cae4e5428fd42
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-23 14:17:40 -0500

    [Vista historial estudiante][datos del estudiante][programas]

--- resources/views/estudiante/historial.blade.php
+++ resources/views/estudiante/historial.blade.php
@@ -102,9 +102,9 @@
             <ul class="nav nav-tabs taps_programas" role="tablist">
                 @for($i = 0; $i < count($programas); $i++)
                     @if($i == 0)
-                        <li class="nav-item active"><a class="nav-link active" data-toggle="pill" href="#tap_0" role="tab" aria-controls="pills-contact" aria-selected="false">$programas[$i]['programa']</a></li>
+                        <li class="nav-item active"><a class="nav-link active" data-toggle="pill" href="#tap_0" role="tab" aria-controls="pills-contact" aria-selected="false">{{ $programas[$i]['programa'] }}</a></li>
                     @else
-                        <li class="nav-item "><a class="nav-link " data-toggle="pill" href="#tap_1" role="tab" aria-controls="pills-contact" aria-selected="false">$programas[$i]['programa']</a></li>
+                        <li class="nav-item "><a class="nav-link " data-toggle="pill" href="#tap_1" role="tab" aria-controls="pills-contact" aria-selected="false">{{ $programas[$i]['programa'] }}</a></li>
                     @endif
                 @endfor
             </ul>

d341cb4b01e29942f2ca424e829b9bda676ed4a8
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-08-23 16:18:56 -0500

    [Vista historial estudiante][datos del estudiante][programas]

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -238,13 +238,13 @@
                     //console.log(consultaProgramas(data.homologante));
                     if (length > 0) {
                         for (let i = 0; i < length; i++) {
-                            /*if (i == 0) {
+                            if (i == 0) {
                                 $('#programas').append(`<li class="nav-item active">
                                 <a class="nav-link active" data-toggle="pill" href="#tap_0" role="tab" aria-controls="pills-contact" aria-selected="true">${data[i]['programa']}</a>
                                 </li>`)
-                            } else {*/
+                            } else {
                                 $('#programas').append(`<li class="nav-item "><a class="nav-link" data-toggle="pill" href="#tap_1" role="tab" aria-controls="pills-contact" aria-selected="false">${data[i]['programa']}</a></li>`)
-                            //}
+                            }
                         }
                     } else {
                         $('#programas').html('');

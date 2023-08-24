f74dfba5387e738c72f26a9ce8d6fdf7edcc26bd
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-08-24 11:41:25 -0500

    test

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -175,7 +175,7 @@
             if (codBanner.val() != '') {
                 $('#info').html('');
                 $('#programas').html('');
-                $('#info').empty();
+                $('#myTabs').empty();
                 consultaEstudiante(codBanner.val());
                 consultaNombre(codBanner.val());
 

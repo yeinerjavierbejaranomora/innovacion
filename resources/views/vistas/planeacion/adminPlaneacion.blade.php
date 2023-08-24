c85d4a8e9529d199932cec6827894ca07db0008d
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-24 15:32:08 -0500

    []

--- resources/views/vistas/planeacion/adminPlaneacion.blade.php
+++ resources/views/vistas/planeacion/adminPlaneacion.blade.php
@@ -49,12 +49,24 @@
 
     #botonModalTiposEstudiantes,
     #botonModalProgramas,
-    #botonModalOperador,
-    #botondataTable {
+    #botonModalOperado
+     {
         background-color: #dfc14e;
         border-color: #dfc14e;
         color: white;
         width: 100px;
+        height: 30px;
+        border-radius: 10px;
+        font-weight: bold;
+        place-items: center;
+        font-size: 14px;
+    }
+
+    #botondataTable{
+        background-color: #dfc14e;
+        border-color: #dfc14e;
+        color: white;
+        width: 2500px;
         height: 30px;
         border-radius: 10px;
         font-weight: bold;

4820412fe95a7f4fd6b025846fae2b1e2d3e34ce
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-25 09:55:51 -0500

    [acordion facultades]

--- resources/views/vistas/planeacion/adminPlaneacion.blade.php
+++ resources/views/vistas/planeacion/adminPlaneacion.blade.php
@@ -270,10 +270,10 @@
 
                 <div class="col-4 text-start">
                     <div class="card shadow mb-5" id="cardFacultades">
-                        <div class="card-header" id="HeadingFacultades" style="width:100%; cursor:pointer;" data-toggle="collapse" data-target="#acordionFacultades" aria-expanded="true" aria-controls="acordionFacultades">
+                        <div class="card-header" id="HeadingFacultades" style="width:100%; cursor:pointer;" data-toggle="collapse" data-target="#acordionFacultades" aria-expanded="false" aria-controls="acordionFacultades">
                             <h5><strong>Seleccionar Facultades</strong></h5>
                         </div>
-                        <div class="card-body text-start" id="acordionFacultades" class="collapse shadow" aria-labelledby="HeadingFacultades">
+                        <div class="card-body text-start collapse shadow" id="acordionFacultades" aria-labelledby="HeadingFacultades">
                             <div name="facultades" id="facultades"></div>
                         </div>
                         <div class="card-footer text-center" style="height: 55px;">

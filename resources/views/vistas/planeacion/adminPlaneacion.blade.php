42a12a8a71e4072d6f6f113b25ce276bfb597e36
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-08-30 09:32:34 -0500

    test scroll

--- resources/views/vistas/planeacion/adminPlaneacion.blade.php
+++ resources/views/vistas/planeacion/adminPlaneacion.blade.php
@@ -117,6 +117,12 @@
         background: #FFFFFF;
     }
 
+    canvas {
+    overflow-clip-margin: content-box;
+    overflow: clip;
+    overflow-x: auto;
+}
+
 </style>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>

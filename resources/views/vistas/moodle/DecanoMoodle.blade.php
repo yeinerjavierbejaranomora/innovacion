559f571bfe09700fab443c16fbc175e22c064b65
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-01 14:43:53 -0500

    debug

--- resources/views/vistas/moodle/DecanoMoodle.blade.php
+++ resources/views/vistas/moodle/DecanoMoodle.blade.php
@@ -513,7 +513,7 @@
             }
 
             function programas() {
-                var programasSeleccionadosAux = [];
+                console.log(facultadesSeleccionadas);
                 var formData = new FormData();
                 var array = [];
                 for (const key in facultadesSelect) {

d40002f3254c4f9d2e037fb71e7c6301eb853d9b
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-01 10:21:08 -0500

    test

--- resources/views/vistas/mafi/Decano.blade.php
+++ resources/views/vistas/mafi/Decano.blade.php
@@ -626,6 +626,7 @@
                             });
                         }
                     })
+                    var programasSeleccionados = [programasSeleccionados];
                     console.log(programasSeleccionados);
             }
 

57d377802f4997d2629e09f4058e107510279698
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-04 15:21:34 -0500

    [buttons]

--- resources/views/vistas/planeacion/DecanoPlaneacion.blade.php
+++ resources/views/vistas/planeacion/DecanoPlaneacion.blade.php
@@ -691,6 +691,46 @@
                 $('#periodos input[type="checkbox"]').prop('checked', true);
             });
 
+            $('#deshacerFacultades').on('click', function(e) {
+                $('#facultades input[type="checkbox"]').prop('checked', false);
+            });
+
+            $('#seleccionarFacultades').on('click', function(e) {
+                $('#facultades input[type="checkbox"]').prop('checked', true);
+            });
+
+            $("#todosContinua").change(function() {
+                if ($(this).is(":checked")) {
+                    $("#Continua input[type='checkbox']").prop("checked", true);
+                } else {
+                    $("#Continua input[type='checkbox']").prop("checked", false);
+                }
+            });
+
+            $("#todosPregrado").change(function() {
+                if ($(this).is(":checked")) {
+                    $("#Pregrado input[type='checkbox']").prop("checked", true);
+                } else {
+                    $("#Pregrado input[type='checkbox']").prop("checked", false);
+                }
+            });
+
+            $("#todosEsp").change(function() {
+                if ($(this).is(":checked")) {
+                    $("#Esp input[type='checkbox']").prop("checked", true);
+                } else {
+                    $("#Esp input[type='checkbox']").prop("checked", false);
+                }
+            });
+
+            $("#todosMaestria").change(function() {
+                if ($(this).is(":checked")) {
+                    $("#Maestria input[type='checkbox']").prop("checked", true);
+                } else {
+                    $("#Maestria input[type='checkbox']").prop("checked", false);
+                }
+            });
+
             function vistaEntrada() {
                 var key = Object.keys(facultadesSelect);
                 var cantidadFacultades = key.length;

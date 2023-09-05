9a58135c3d7333c8461f317c5573a5ae71ef2a7f
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-05 16:35:35 -0500

    [vista Mafi]

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -118,6 +118,11 @@
     #seccion {
         background: #FFFFFF;
     }
+
+    .activo {
+        
+    }
+
 </style>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>

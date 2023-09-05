3bf18763328b7c33f5604ce2c27cdac71ee776ef
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-05 14:40:53 -0500

    debug

--- app/Http/Controllers/InformeMafiController.php
+++ app/Http/Controllers/InformeMafiController.php
@@ -70,6 +70,42 @@ class InformeMafiController extends Controller
                 ->select(DB::raw('COUNT(sello) AS TOTAL, sello, autorizado_asistir'))
                 ->groupBy('sello')
                 ->get();
+
+            $consulta = DB::table('datosMafi')
+            ->where('estado', 'Activo')
+            ->select('sello', 'autorizado_asistir')
+            ->get();
+
+
+            $selloFinanciero = 0;
+            $Retencion = 0;
+            $AFP = 0;
+            $Vacio = 0;
+
+
+            foreach($consulta as $dato){
+                $sello = $dato->sello;
+                $estado = $dato->autorizado_asistir;
+
+                if($sello == 'TIENE SELLO FINANCIERO')
+                {
+                    $selloFinanciero =+1;
+                }
+
+                if($sello == 'TIENE RETENCION' && empty($estado) ){
+                    $AFP =+ 1;
+                }
+     
+                if($sello == 'TIENE RETENCION' && !empty($estado) ){
+                    $Retencion =+ 1;
+                }
+
+                if($sello == 'NO EXISTE'){
+                    $Vacio =+1;
+                }
+            }
+
+            dd($selloFinanciero, $AFP, $Retencion, $Vacio);
         }
 
         if ($tabla == 'planeacion') {

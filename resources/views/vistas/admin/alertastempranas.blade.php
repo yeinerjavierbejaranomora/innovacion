0c9e0173049f46a32a0e5ed9df50286fb63f123c
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-06 12:18:20 -0500

    [creacion vista inicio de alertas tempranas][mod card filtros periodo][checkbox]

--- resources/views/vistas/admin/alertastempranas.blade.php
+++ resources/views/vistas/admin/alertastempranas.blade.php
@@ -85,7 +85,7 @@
                                                     }
 
                                                     .checkbox-wrapper-49 label {
-                                                      width: var(--size);
+                                                      width: var(--size)/0.5;
                                                       height: calc(var(--size) / 2);
                                                       box-sizing: border-box;
                                                       border: 3px solid;

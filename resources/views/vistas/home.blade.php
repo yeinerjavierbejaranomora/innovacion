d3d6f1ea50cc95268599709fa486147d29ff3ead
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-06 08:54:19 -0500

    [cards]

--- resources/views/vistas/home.blade.php
+++ resources/views/vistas/home.blade.php
@@ -82,7 +82,9 @@
                         <div class="card-header">
                             <h5><strong>Admisiones</strong></h5>
                         </div>
-                        
+                        <div class="card-body">
+                            
+                        </div>
                     </div>
 
                 </div>

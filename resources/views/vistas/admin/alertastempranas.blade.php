2c602099b5753fbaabd0b3dcb136a109d0b7e51e
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-06 12:13:24 -0500

    [creacion vista inicio de alertas tempranas][mod card filtros periodo][checkbox]

--- resources/views/vistas/admin/alertastempranas.blade.php
+++ resources/views/vistas/admin/alertastempranas.blade.php
@@ -60,6 +60,58 @@
                                         </div> --}}
                                         <div class="col-xl-2 text-center">
                                             <h5 id="tituloNiveles"><strong>Pregrado Cuatrimestral</strong></h5>
+                                            <div class="card-body periodos" style="width:100%;" id="Continua">
+                                                <div class="checkbox-wrapper-41">
+                                                    <input type="checkbox">
+                                                  </div>
+
+                                                  <style>
+                                                    .checkbox-wrapper-41 {
+                                                      --size: 100px;
+                                                    }
+
+                                                    .checkbox-wrapper-41 input[type="checkbox"] {
+                                                      -webkit-appearance: none;
+                                                      width: var(--size);
+                                                      height: calc(var(--size) / 2);
+                                                      background-color: #fff;
+                                                      border: 3px solid #222;
+                                                      border-radius: 30px 100px 100px 100px;
+                                                      box-shadow: 0 10px 20px rgba(0,0,0,0.2);
+                                                      outline: none;
+                                                      cursor: pointer;
+                                                      position: relative;
+                                                      transition: all 0.5s;
+                                                    }
+
+                                                    .checkbox-wrapper-41 input[type="checkbox"]::before {
+                                                      content: "";
+                                                      position: absolute;
+                                                      width: calc(var(--size) / 2);
+                                                      height: calc(var(--size) / 2);
+                                                      left: 0;
+                                                      top: 50%;
+                                                      transform: translateY(-50%) scale(0.7);
+                                                      border: 3px solid #222;
+                                                      border-radius: 30px 100px 100px 100px;
+                                                      background-color: #fde881;
+                                                      box-sizing: border-box;
+                                                      transition: all 0.5s;
+                                                    }
+
+                                                    .checkbox-wrapper-41 input[type="checkbox"]:checked {
+                                                      background-color: #fde881;
+                                                      border-radius: 100px 100px 30px 100px;
+                                                    }
+
+                                                    .checkbox-wrapper-41 input[type="checkbox"]:checked::before {
+                                                      left: 50%;
+                                                      background-color: #fff;
+                                                      border-radius: 100px 100px 30px 100px;
+                                                    }
+                                                  </style>
+
+                                            </div>
                                         </div>
                                         <div class="col-xl-2 text-center">
                                             <h5 id="tituloNiveles"><strong>Pregrado Semestral</strong></h5>

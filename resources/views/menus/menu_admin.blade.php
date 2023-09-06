23e9c76d8dc4565cc8131d15b679f776875cfd2f
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-06 11:29:48 -0500

    [creacion vista inicio de alertas tempranas][crear controller y rutas]

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -64,7 +64,7 @@
         </div>
     </li>
     <li class="nav-item">
-        <a href="" class="nav-link" href="{{ route('alertas.inicio') }}">
+        <a  class="nav-link" href="{{ route('alertas.inicio') }}">
             <i class="fa-solid fa-bell"></i>
             <span>Alertas Tempranas<br>(Programación-Planeación)</span></a>
         </a>

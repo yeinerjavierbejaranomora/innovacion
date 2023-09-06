08c2df1084034d69effebea795bca3c0b8454554
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-06 12:53:22 -0500

    [creacion vista inicio de alertas tempranas][mod card filtros periodo][checkbox]

--- resources/views/vistas/admin/alertastempranas.blade.php
+++ resources/views/vistas/admin/alertastempranas.blade.php
@@ -149,8 +149,8 @@
                                                     </label>
                                                 </div>
                                                 <div class="checkbox-wrapper-46">
-                                                    <input class="inp-cbx" id="cbx-46" type="checkbox" />
-                                                    <label class="cbx" for="cbx-46"><span>
+                                                    <input class="inp-cbx" id="cbx-47" type="checkbox" />
+                                                    <label class="cbx" for="cbx-47"><span>
                                                             <svg width="12px" height="10px" viewbox="0 0 12 10">
                                                                 <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
                                                             </svg></span><span>202316</span>

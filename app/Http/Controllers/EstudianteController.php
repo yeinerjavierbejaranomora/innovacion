aae2ab05eb79118b085cb9f0722ecdde8f7e0f75
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-09-06 08:51:35 -0500

    test

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -116,6 +116,8 @@ class EstudianteController extends Controller
         /*utilizamos la función array_filter() y in_array() para filtrar los elementos de $array1 que existen en $array2. El resultado se almacena en $intersection. Luego, verificamos si $intersection contiene al menos un elemento utilizando count($intersection) > 0.*/
 
         foreach ( $mallaCurricular as $key_mallaCurricular => $value_mallaCurricular) {
+            dd($value_mallaCurricular);
+            exit;
             $materias_malla[]=$value_mallaCurricular->idCurso;
         }
                     

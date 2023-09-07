9e3bf618923e456e439ace6892853ce7c42b2ccb
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-07 11:13:04 -0500

    [modificacion vista inicio de alertas tempranas][tabla][ajax traer data alertas][cocnuslta por programas]

--- resources/views/vistas/admin/alertastempranas.blade.php
+++ resources/views/vistas/admin/alertastempranas.blade.php
@@ -515,6 +515,7 @@
         $('#colTabla').removeClass('hidden');
         var url, data;
         var table;
+        console.log(periodos);
         console.log(programasSeleccionados);
         if (programasSeleccionados.length > 0) {
             url = "{{ route('alertas.tabla.programa')}}",

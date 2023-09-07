6f8f5fab1be221fac46bc31a08049205dd0c6419
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-07 10:51:29 -0500

    [modificacion vista inicio de alertas tempranas][tabla][ajax traer data alertas][cocnuslta por programas]

--- app/Http/Controllers/AlertasTempranasController.php
+++ app/Http/Controllers/AlertasTempranasController.php
@@ -23,7 +23,7 @@ class AlertasTempranasController extends Controller
                         ->whereIn('periodo',$periodos)
                         ->whereIn('codprograma',$programas)
                         ->orderBy('created_at','desc')
-                        ->dd();
+                        ->toSql();
         var_dump($consultaAlertas);die();
     }
 

c78005a217c7f271dc7b49f98e21e3f8fd60fdbc
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-07 09:56:06 -0500

    test

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -315,7 +315,7 @@
                         <h5 id="tituloEstudiantes"><strong>Total estudiantes Banner</strong></h5>
                         <h5 class="tituloPeriodo"><strong></strong></h5>
                         <span data-toggle="tooltip" title="Este gráfico muestra el total de los estudiantes y los clásifica en activos e inactivos. Adicionalmente cuenta con la opción 'Descargar datos Banner' la cual genera un Excel con los datos de Banner.">
-                                <i class="fa-solid fa-circle-question"></i>
+                        <button type="button" class="btn btn-warning" data-toggle="tooltip" data-placement="bottom"><i class="fa-solid fa-circle-question"></i></button>
                         </span> 
                     </div>
                     <div class="card-body center-chart">

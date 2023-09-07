3a5fb2fd8b2a8c1b1005cf782379c7c6631ff3de
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-07 10:11:33 -0500

    [modificacion vista inicio de alertas tempranas][tabla][ajax traer data alertas][rutas y controlador funcion programas]

--- resources/views/vistas/admin/alertastempranas.blade.php
+++ resources/views/vistas/admin/alertastempranas.blade.php
@@ -532,7 +532,7 @@
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
             type: 'post',
-            url: '{{ route('alertas.tabla.programa')}}',
+            url: "{{ route('alertas.tabla.programa')}}",
             data: data,
             success: function(data) {
                 try {

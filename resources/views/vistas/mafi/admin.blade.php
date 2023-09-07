717a59d4dc3225658d8d9b6086d04442dc755109
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-07 14:35:50 -0500

    test

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -124,6 +124,11 @@
         justify-content: center;
         align-items: center;
     }
+
+    #programas {
+    display: flex;
+    flex-direction: column;
+    }
 </style>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>

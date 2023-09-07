a6de28486cafbcaec9ee19772d15cf44d9f53300
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-07 12:40:59 -0500

    test

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -624,12 +624,12 @@
             var buscador = $('#buscadorProgramas');
             var listaProgramas = $('#programas');
 
-            
+
             // Agregar un evento de escucha al campo de búsqueda
             buscador.on('input', function() {
                 var query = $(this).val().toLowerCase();
 
-                $listaProgramas.find('label').each(function() {
+                listaProgramas.find('label').each(function() {
                     var $label = $(this);
                     var $checkbox = $label.find('input[type="checkbox"]');
                     var programa = $checkbox.val().toLowerCase();

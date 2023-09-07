ec6174a8e1c3a1f1985e998e28289e589831c66d
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-07 09:10:52 -0500

    [modificacion vista inicio de alertas tempranas][tabla][debbug programas]

--- resources/views/vistas/admin/alertastempranas.blade.php
+++ resources/views/vistas/admin/alertastempranas.blade.php
@@ -474,21 +474,13 @@
             array[index] = '2023' + periodo;
         });
         if (periodosSeleccionados.length > 0) {
-            console.log(totalProgramas);
+            //console.log(totalProgramas);
             if ($('#programas input[type="checkbox"]:checked').length > 0 && $('#programas input[type="checkbox"]:checked').length < totalProgramas) {
                 var checkboxesProgramas = $('#programas input[type="checkbox"]:checked');
                 programasSeleccionados = [];
                 checkboxesProgramas.each(function() {
                     programasSeleccionados.push($(this).val());
                 });
-                destruirTable();
-                Contador();
-                var periodosSeleccionados = getPeriodos();
-                periodosSeleccionados.forEach(function(periodo, index, array) {
-                    array[index] = '2023' + periodo;
-                });
-                var periodos = getPeriodos();
-                dataTable(periodos);
             }else{
                 if ($('#facultades input[type="checkbox"]:checked').length > 0) {
                     var checkboxesSeleccionados = $('#facultades input[type="checkbox"]:checked');
@@ -503,6 +495,14 @@
                     facultadesSeleccionadas = [];
                 }
             }
+            destruirTable();
+            Contador();
+            var periodosSeleccionados = getPeriodos();
+            periodosSeleccionados.forEach(function(periodo, index, array) {
+                array[index] = '2023' + periodo;
+            });
+            var periodos = getPeriodos();
+            dataTable(periodos);
         }else{
             programasSeleccionados = [];
             facultadesSeleccionadas = [];

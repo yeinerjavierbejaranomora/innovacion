2743a4da7c078449b08d953ce81a8dd88e4b4786
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-08 12:21:53 -0500

    [css]

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -143,10 +143,6 @@
         background-color: #DFE0E2;
     }
 
-    .inputTodos:checked {
-    background-color: #dfc14e;
-    }
-
 </style>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>

c1aa44bfd4f23514ff8a4eda7c0f2aca855e6135
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-09-08 16:38:07 -0500

    [modificacion vista inicio de alertas tempranas][cambios chechbox flitros][prueba estilos checkbox][periodos formacion continua][estilos]

--- resources/views/vistas/admin/alertastempranas.blade.php
+++ resources/views/vistas/admin/alertastempranas.blade.php
@@ -486,7 +486,7 @@
                     if (periodo.nivelFormacion == "EDUCACION CONTINUA") {
                         //$('#Continua').append(`<label"> <input type="checkbox" value="${periodo.periodo}" checked> ${periodo.periodo}</label><br>`);
                         $('#Continua').append(`<div class="checkbox-wrapper mb-1">
-                                        <input class="inp-cbx" id="cbx-${periodo.periodo}" type="checkbox" value="${periodo.periodo}"/>
+                                        <input class="inp-cbx" id="cbx-${periodo.periodo}" type="checkbox" value="${periodo.periodo}" checked>
                                         <label class="cbx" for="cbx-${periodo.periodo}"><span>
                                                 <svg width="12px" height="10px" viewbox="0 0 12 10">
                                                     <polyline points="1.5 6 4.5 9 10.5 1"></polyline>

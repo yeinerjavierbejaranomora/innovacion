3d4b681a2c79123da27d41426befbae409288596
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-08 12:17:59 -0500

    test

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -143,6 +143,10 @@
         background-color: #DFE0E2;
     }
 
+    .todos-checked {
+    background-color: #dfc14e;
+    }
+
 </style>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>

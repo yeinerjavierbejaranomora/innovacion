e4e66f7bfd37b7e7a6388fb39c56ef8631fcd6a8
JuanPabloQuitian <136613467+JuanPabloQuitian@users.noreply.github.com>
2023-09-08 10:24:00 -0500

    [modificacion checkbox vista mafi admin]

--- resources/views/vistas/mafi/admin.blade.php
+++ resources/views/vistas/mafi/admin.blade.php
@@ -639,12 +639,13 @@
             buscador.on('input', function() {
                 var query = $(this).val().toLowerCase();
                 divProgramas.find('label').each(function() {
-                    var label = $(this);
                     //console.log(label);
                     var etiqueta = label.text().toLowerCase();
                     var $checkbox = label.find('input[type="checkbox"]');
 
                     if (etiqueta.includes(query)) {
+                        var label = $('#Checkbox+'$checkbo.val()'+'));
+                        console.log(label);
                         //label.removeClass('d-none');
                         label.removeAttr('hidden');
                         //$checkbox.removeClass('d-none');

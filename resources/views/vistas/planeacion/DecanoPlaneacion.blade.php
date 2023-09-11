220cd7187666132f3e259efb9e96373e2df14435
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-11 18:32:07 -0500

    [Títulos]

--- resources/views/vistas/planeacion/DecanoPlaneacion.blade.php
+++ resources/views/vistas/planeacion/DecanoPlaneacion.blade.php
@@ -958,7 +958,7 @@
             }
 
             function limpiarTitulos() {
-                var elementosTitulos = $('#tituloEstudiantes, #tituloEstadoFinanciero, #tituloRetencion, #tituloEstudiantesNuevos, #tituloTipos, #tituloOperadores, #tituloEstudiantesAntiguos').find("strong");
+                var elementosTitulos = $('#tituloEstudiantes, #tituloEstadoFinanciero, #tituloRetencion, #tituloEstudiantesNuevos, #tituloTipos, #tituloOperadores, #tituloEstudiantesAntiguos, #tituloProgramas').find("strong");
                 var parteEliminar = ': ';
                 elementosTitulos.each(function() {
                     var contenidoActual = $(this).text();

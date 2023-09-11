68d77b846812b2737dcc14b6d21431892d3ddaba
Yeiner javier bejarano mora <yeiner.bejarano@ibero.edu.co>
2023-09-11 14:45:58 -0500

    tetz

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -207,7 +207,7 @@
                             <div class="card-body">
                                 <h5 class="card-title">Special title treatment</h5>
                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              
                             </div>
                             </div>
                      
@@ -216,35 +216,35 @@
                             <div class="card-body">
                                 <h5 class="card-title">Special title treatment</h5>
                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              
                             </div>
                             </div>
                             <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">Special title treatment</h5>
                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              
                             </div>
                             </div>
                             <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">Special title treatment</h5>
                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              
                             </div>
                             </div>
                             <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">Special title treatment</h5>
                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              
                             </div>
                             </div>
                             <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">Special title treatment</h5>
                                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                              
                             </div>
                             </div>
                        

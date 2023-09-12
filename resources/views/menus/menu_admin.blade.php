20153f9bed88cb5231123a2a8e5d0b30e1e78c05
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-12 09:09:50 -0500

    [vista menu Admin][numero de alertasTemparnas][route controller]

--- resources/views/menus/menu_admin.blade.php
+++ resources/views/menus/menu_admin.blade.php
@@ -76,8 +76,8 @@
     numeroAlertas();
     function numeroAlertas(){
         $.get("{{ route('alertas.notificaciones') }}",{},function(data){
+            console.log(data);
             var total = data[0].total_alertas;
-            console.log(total);
             if (total > 99) {
                 $('#notificacionesCount').append('+99');
             } else {

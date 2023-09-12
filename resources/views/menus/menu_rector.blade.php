1294b582f724941d826b1f9fd424eb5af37655d4
JuanPabloQuitian <51885809+juanp11@users.noreply.github.com>
2023-09-12 10:40:46 -0500

    [vista menu rector][numero de alertasTemparnas]

--- resources/views/menus/menu_rector.blade.php
+++ resources/views/menus/menu_rector.blade.php
@@ -48,8 +48,41 @@
             <span>Planeación</span></a>
     </li>
     <li class="nav-item" id="menuAlertas">
-        @include('layout.alertas');
-    </li>
+        <style>
+            .notificaciones-count {
+            background-color: red;
+            color: white;
+            font-size: 12px;
+            padding: 2px 6px;
+            border-radius: 50%;
+            position: absolute;
+            vertical-align: middle;
+            right: 0;
+        }
+        </style>
+        <script>
+            numeroAlertas();
+            function numeroAlertas(){
+                id_rol = '{{ auth()->user()->id_rol }}';
+                alert(id_rol);
+                if (id_rol == 9 || id_rol == 19 || id_rol ==20) {
+                    $.get("{{ route('alertas.notificaciones') }}",{},function(data){
+                        var total = data;
+                        if (total > 99) {
+                            $('#notificacionesCount').append('+99');
+                        } else {
+                            $('#notificacionesCount').append(`${total}`);
+                        }
+                    })
+                }
+            }
+        </script>
+                <a  class="nav-link" href="{{ route('alertas.inicio') }}">
+                    <i class="fa-solid fa-bell"></i>
+                    <span>Alertas Tempranas<br>(Programación-Planeación)</span>
+                    <span id="notificacionesCount" class="notificaciones-count"></span>
+                </a>
+            </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">

8f4f4c847a8c5ae1ca68cb479d0885abb982db8b
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-09-13 16:26:43 -0500

    test

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -506,7 +506,7 @@ div .show{
                                     }
 
                                     // Agrega la materia como una columna en la fila actual
-
+                                    var $filaMateria="";
 $filaMateria+='<td style="color:white;margin-right: 1%">';
 $filaMateria+='  <div class="card " style=" background-color:transparent;border: none;">'
 $filaMateria+='    <div class="card card_historial materias">'

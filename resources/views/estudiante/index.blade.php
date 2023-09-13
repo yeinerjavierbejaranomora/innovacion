3d524f61e7fb4dc32f43de8fd02c160ad1e848c7
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-09-13 11:41:42 -0500

    test

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -99,9 +99,13 @@
 .datos{
     color: white;
     
+  
+
+}
+
+.tab-pane .fade .datos .active .show{
     padding-top: 22px;
     padding-bottom: 40px;
-
 }
 </style>
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>

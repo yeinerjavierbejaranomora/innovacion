aa775baf9d189e3af0639c92eeed7aa936c43c55
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-09-13 10:05:13 -0500

    test

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -636,7 +636,7 @@
                         .attr('data-programa',tab.cod_programa)
                         .attr('data-tap', 'tab' + index)
                         .attr('href', '#tab' + index)
-                        .attr('id', '#tab_li' + index)
+                        .attr('id', 'tab_li' + index)
                         .text(tab.programa); // Suponiendo que cada objeto tiene una propiedad 'title'
 
                         // Agregar la pestaña a la lista de pestañas
@@ -655,7 +655,7 @@
                         .attr('data-id',codBanner )
                         .attr('data-programa',tab.cod_programa)
                         .attr('data-tap', 'tab' + index)
-                        .attr('href', 'tab' + index)
+                        .attr('href', '#tab' + index)
                         .text(tab.programa);
                         
                         $(tabContent).append()

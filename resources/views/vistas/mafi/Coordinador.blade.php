c6262064646fa158ffa863dbee025274925fd8bc
Ruben Charry <ruben.charry@iberoamericana.edu.co>
2023-09-14 10:22:24 -0500

    test

--- resources/views/vistas/mafi/Coordinador.blade.php
+++ resources/views/vistas/mafi/Coordinador.blade.php
@@ -176,7 +176,7 @@
             <br>
             <div class="text-center" id="mensaje">
                 <h3>A continuación podrás visualizar los datos de tus Programas:
-                    <?php dd($programas); ?>
+                    <?php dd($programas->codprograma); ?>
                     @foreach ($programas as $programa)
                     {{$programa->codprograma}} -
                     @endforeach

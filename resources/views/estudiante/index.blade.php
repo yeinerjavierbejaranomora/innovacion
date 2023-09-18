5adb5aac7272976a3bd798d823fde5fb4e454e83
yeiner bejarano <yeiner.bejarano@gmail.com>
2023-09-18 12:42:19 -0500

    test

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -510,7 +510,7 @@ div .show{
 $filaMateria+='<td style="color:white;margin-right: 1%">';
 $filaMateria+='  <div class="card " style=" background-color:transparent;border: none;width:162px">'
 $filaMateria+='    <div class="card card_historial materias" style="height: 297px;">'
-$filaMateria+='      <div class="card-body">'
+$filaMateria+='      <div class="card-body" style="padding: 0.5rem;">'
 $filaMateria+='        <div class="" style=" display: flex;color: black;">'
 $filaMateria+='          <span class="'+materia.color+'" style="border-bottom: 2px solid;border-top: 2px solid;border-left:2px solid;border-top-left-radius: 30px;border-bottom-left-radius: 28px;min-width: 27px;height: 42px;">&nbsp;&nbsp;&nbsp;<br><br>'
 $filaMateria+='          </span>'
